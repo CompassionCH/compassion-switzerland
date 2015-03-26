4ae5f4c02349fcde99ee2bc27fea05f99e5d5855
David Coninckx <davconinckx@gmail.com>
2015-03-26 10:36:34 +0100

    Fix PEP8 code style on {}
    Remove useless code

--- child_sync_typo3/wizard/child_depart_wizard.py
+++ child_sync_typo3/wizard/child_depart_wizard.py
@@ -21,11 +21,8 @@ class end_sponsorship_wizard(orm.TransientModel):
 
         wizard = self.browse(cr, uid, ids[0], context)
         child = wizard.child_id
-        vals = {'exit_date': wizard.end_date,
-                'state': 'F'}
 
         if child.state == 'I':
             res = child.child_remove_from_typo3()
-        child.write(vals)
 
         return res or Sync_typo3.typo3_index_error(cr, uid, self, context)
