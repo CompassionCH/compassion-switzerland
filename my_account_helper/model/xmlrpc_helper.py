90232b29a4f79d7801acdca8af6edf2509579cad
Emmanuel Mathier <emmanuel.mathier@gmail.com>
2015-04-14 11:00:42 +0200

    Rename childs to children
    Test if the contract is linked to a child
    
    Remove partner_compassion and recurring_contract from dependencies

--- my_account_helper/model/xmlrpc_helper.py
+++ my_account_helper/model/xmlrpc_helper.py
@@ -16,7 +16,7 @@ class xmlrpc_helper(orm.Model):
     _inherit = "res.partner"
 
     # return all child information for all childs from a partner_id specified
-    def get_childs_from_partner(self, cr, uid, partner_id, context=None):
+    def get_children_from_partner(self, cr, uid, partner_id, context=None):
 
         # get the Singleton instance of the recurring.contract model from the
         # registry pool for the database in use
@@ -26,22 +26,24 @@ class xmlrpc_helper(orm.Model):
         contract_ids = contract_obj.search(
             cr, uid, [('partner_id', '=', partner_id)], context=None)
 
-        # get all child information from each contract_id for each child
-        childs = list()
+        # get all child informations from each contract_id for each child
+        children = list()
         for contract in contract_obj.browse(cr, uid, contract_ids, context):
-            child = dict()
-            child['id'] = contract.child_id.id
-            child['code'] = contract.child_id.code
-            child['type'] = contract.child_id.type
-            child['state'] = contract.child_id.state
-            child['firstname'] = contract.child_id.firstname
-            child['name'] = contract.child_id.name
-            child['gender'] = contract.child_id.gender
-            child['birthdate'] = contract.child_id.birthdate
-            child['desc_fr'] = contract.child_id.desc_fr
-            child['desc_it'] = contract.child_id.desc_it
-            child['desc_de'] = contract.child_id.desc_de
-            child['desc_en'] = contract.child_id.desc_en
-            childs.append(child)
+            #contract must be linked to a child
+            if contract.child_id:
+                child = dict()
+                child['id'] = contract.child_id.id
+                child['code'] = contract.child_id.code
+                child['type'] = contract.child_id.type
+                child['state'] = contract.child_id.state
+                child['firstname'] = contract.child_id.firstname
+                child['name'] = contract.child_id.name
+                child['gender'] = contract.child_id.gender
+                child['birthdate'] = contract.child_id.birthdate
+                child['desc_fr'] = contract.child_id.desc_fr
+                child['desc_it'] = contract.child_id.desc_it
+                child['desc_de'] = contract.child_id.desc_de
+                child['desc_en'] = contract.child_id.desc_en
+                childs.append(child)
 
-        return childs
+        return children
