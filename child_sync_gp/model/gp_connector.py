b0dd780844ff6e8e5688cd421a814f882b26288e
ecino <ecino@compassion.ch>
2015-04-14 16:45:56 +0200

    replace trim by strip

--- child_sync_gp/model/gp_connector.py
+++ child_sync_gp/model/gp_connector.py
@@ -41,7 +41,7 @@ class GPConnect(mysql_connector):
     def upsert_child(self, uid, child):
         """Push or update child in GP after converting all relevant
         information in the destination structure."""
-        name = trim(child.name.replace(child.firstname, '', 1))
+        name = strip(child.name.replace(child.firstname, '', 1))
         vals = {
             'CODE': child.code,
             'NOM': name,
