90c8da04c6e1d68b1338e714e24b301b0390cb28
ecino <ecino@compassion.ch>
2015-04-24 11:32:39 +0200

    Remove unused gmc_states for Switzerland.

--- sponsorship_switzerland/model/contracts.py
+++ sponsorship_switzerland/model/contracts.py
@@ -18,11 +18,39 @@ class contracts(orm.Model):
 
     def _get_gmc_states(self, cr, uid, context=None):
         """ Adds a new gmc state for tracking sponsorships for which we have
-        to order the new picture of the child. """
-        states = super(contracts, self)._get_gmc_states(cr, uid, context)
-        states.insert(2, ('order_picture', _('Order picture')))
-        return states
+        to order the new picture of the child. Remove 'casestudy' and
+        'picture' states which are useless for Switzerland."""
+        return [
+            ('order_picture', _('Order Picture')),
+            ('biennial', _('Biennial')),
+            ('depart', _('Child Departed')),
+            ('transfer', _('Child Transfer'))]
 
     def new_biennial(self, cr, uid, ids, context=None):
         """ Called when new picture and new case study is available. """
         self.write(cr, uid, ids, {'gmc_state': 'order_picture'}, context)
+
+    def set_gmc_event(self, cr, uid, ids, event, context=None):
+        """
+        Called when a Child Update was received for a sponsored child.
+        Arg event can have one of the following values :
+            - Transfer : child was transferred to another project
+            - CaseStudy : child has a new casestudy
+            - NewImage : child has a new image
+
+        We handle only the Transfer event, as other events are not relevant
+        for Switzerland.
+        """
+        if event == 'Transfer':
+            return self.write(cr, uid, ids, {'gmc_state': event.lower()},
+                              context)
+        return True
+
+    # Called only at module installation
+    def migrate_contracts(self, cr, uid, context=None):
+        """ Remove no more used gmc_states. """
+        cr.execute("""
+            UPDATE recurring_contract SET gmc_state = NULL
+            WHERE gmc_state IN ('picture', 'casestudy')
+        """)
+        return True
