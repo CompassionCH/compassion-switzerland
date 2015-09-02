95619d6c92f519834b873eb094f6ccc3abcfbc70
Emmanuel Mathier <emmanuel.mathier@gmail.com>
2015-09-02 14:10:22 +0200

    Rename modul import_spoken_langs_for_partner_and_country_sync_gp to sbc_sync_gp

--- import_spoken_langs_for_partner_and_country_sync_gp/models.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from openerp import models, fields, api
-
-# class import_spoken_langs_for_partner_and_country_sync_gp(models.Model):
-#     _name = 'import_spoken_langs_for_partner_and_country_sync_gp.import_spoken_langs_for_partner_and_country_sync_gp'
-
-#     name = fields.Char()
