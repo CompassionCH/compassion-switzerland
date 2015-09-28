e35058670e1a16b5faa889e114bae7c2a6c8c003
Emanuel Cino <ecino@compassion.ch>
2015-09-28 16:42:59 +0200

    Remove pdb

--- child_sync_gp/model/child.py
+++ child_sync_gp/model/child.py
@@ -18,14 +18,12 @@ from tempfile import TemporaryFile
 
 from . import gp_connector
 import base64
-import pdb
 
 
 class child_compassion(orm.Model):
     _inherit = 'compassion.child'
 
     def create(self, cr, uid, vals, context=None):
-        pdb.set_trace()
         new_id = super(child_compassion, self).create(cr, uid, vals, context)
         child = self.browse(cr, uid, new_id, context)
         gp_connect = gp_connector.GPConnect()
@@ -34,7 +32,6 @@ class child_compassion(orm.Model):
 
     def write(self, cr, uid, ids, vals, context=None):
         """Update GP with the last information of the child."""
-        pdb.set_trace()
         if not isinstance(ids, list):
             ids = [ids]
         gp_connect = gp_connector.GPConnect()
