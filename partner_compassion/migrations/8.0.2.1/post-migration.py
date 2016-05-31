7f539234d64c5a6e0f0c02fdd90d42de78bf51ce
Emanuel Cino <ecino@compassion.ch>
2016-05-31 11:45:44 +0200

    [FIX] Birthdate migration issues

---
+++ partner_compassion/migrations/8.0.2.1/post-migration.py
@@ -0,0 +1,20 @@
+# -*- encoding: utf-8 -*-
+##############################################################################
+#
+#    Copyright (C) 2015 Compassion CH (http://www.compassion.ch)
+#    Releasing children from poverty in Jesus' name
+#    @author: Emanuel Cino <ecino@compassion.ch>
+#
+#    The licence is in the file __openerp__.py
+#
+##############################################################################
+
+
+def migrate(cr, version):
+    if not version:
+        return
+
+    cr.execute("""
+    UPDATE res_partner SET birthdate = birthdate_backup;
+    ALTER TABLE res_partner DROP COLUMN birthdate_backup;
+    """)
