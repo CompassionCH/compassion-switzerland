f64843fe7b4d978c94800d57966fecfe83d1d6f1
Emanuel Cino <ecino@compassion.ch>
2016-11-24 17:38:40 +0100

    [ADD] Wordpress sync module

---
+++ child_sync_wp/models/child_compassion.py
@@ -0,0 +1,99 @@
+# -*- encoding: utf-8 -*-
+##############################################################################
+#
+#    Copyright (C) 2014 Compassion CH (http://www.compassion.ch)
+#    Releasing children from poverty in Jesus' name
+#    @author: David Coninckx <david@coninckx.com>
+#
+#    The licence is in the file __openerp__.py
+#
+##############################################################################
+import logging
+
+import sys
+
+from openerp import models, _
+from openerp.exceptions import Warning
+
+from ..tools.wp_sync import WPSync
+
+
+logger = logging.getLogger(__name__)
+
+
+class CompassionChild(models.Model):
+    _inherit = 'compassion.child'
+
+    def child_add_to_wordpress(self):
+
+        # Solve the encoding problems on child's descriptions
+        reload(sys)
+        sys.setdefaultencoding('UTF8')
+
+        possible_states = ['N', 'R', 'Z']
+        valid_children = self.filtered(lambda c: c.state in possible_states)
+        valid_children._set_unsponsored_since()
+
+        for child in valid_children:
+            # Check for descriptions
+            if not (child.desc_de and child.desc_fr and child.desc_it):
+                raise Warning(
+                    _('Missing descriptions for child %s') % child.code)
+            if not (child.project_id.description_fr and
+                    child.project_id.description_de and
+                    child.project_id.description_it):
+                raise Warning(
+                    _('Missing descriptions for project %s') %
+                    child.project_id.code)
+
+            # Check for pictures
+            if not child.fullshot:
+                self.env['compassion.child.pictures'].create({
+                    'child_id': child.id})
+                child = self.browse(child.id)
+                if not child.fullshot:
+                    raise Warning(
+                        _('Child %s has no picture') % child.code)
+
+        wp = WPSync()
+        res = wp.upload_children(valid_children)
+        if res:
+            valid_children.write({'state': 'I'})
+        return res
+
+    def child_remove_from_wordpress(self):
+        valid_children = self.filtered(lambda c: c.state == 'I')
+        wp = WPSync()
+        # return wp.remove_all_children()
+        if wp.remove_children(valid_children):
+            sponsored = valid_children.filtered('has_been_sponsored')
+            sponsored.write({'state': 'R'})
+            (valid_children - sponsored).write({'state': 'N'})
+        return True
+
+    def child_sponsored(self):
+        """ Remove children from the website when they are sponsored. """
+        to_remove_from_web = self.filtered(lambda c: c.state == 'I')
+        to_remove_from_web.child_remove_from_wordpress()
+
+        return super(CompassionChild, self).child_sponsored()
+
+    def depart(self, args):
+        """ add child remove from typo3 (formerly child_depart_wizard.py) """
+        child = self.browse(args.get('object_id'))
+        if child.state == 'I':
+            if not child.child_remove_from_wordpress():
+                raise Warning(
+                    _("Child depart cannot be executed because the child is "
+                      "not removed from Wordpress."))
+
+        return super(CompassionChild, self).depart(args)
+
+    def deallocate(self, args):
+        """ Remove from typo3 when child is deallocated """
+        child = self.browse(args.get('object_id'))
+        if not child.child_remove_from_wordpress():
+            raise Warning(
+                _("Child deallocate cannot be executed because the child is "
+                  "not removed from Wordpress."))
+        return super(CompassionChild, self).deallocate(args)
