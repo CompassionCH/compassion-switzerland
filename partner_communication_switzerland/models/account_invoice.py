2b2183253476b39e64e3806b69ffbd8406178ef3
Emanuel Cino <ecino@compassion.ch>
2017-10-10 11:56:59 +0200

    CO-1135 FIX donation receipts sent to every income.

--- partner_communication_switzerland/models/account_invoice.py
+++ partner_communication_switzerland/models/account_invoice.py
@@ -28,65 +28,6 @@ class AccountInvoice(models.Model):
         readonly=True
     )
 
-    @api.multi
-    def action_invoice_paid(self):
-        """ Generate a Thank you Communication when invoice is a donation
-            (no sponsorship product inside)
-        """
-        res = super(AccountInvoice, self).action_invoice_paid()
-        invoices = self.filtered(
-            lambda i: (not i.communication_id or i.communication_id.state in (
-                'call', 'pending')) and i.invoice_type != 'sponsorship' and
-            (not i.mapped('invoice_line_ids.contract_id') or (
-                i.invoice_type == 'gift' and i.origin !=
-                'Automatic birthday gift'))
-        )
-        if invoices:
-            invoices.generate_thank_you()
-        return res
-
-    @api.multi
-    def write(self, vals):
-        """ When invoice is open again, remove it from donation receipt. """
-        if vals.get('state') == 'open':
-            for invoice in self.filtered(
-                    lambda i: i.state == 'paid' and i.communication_id and
-                    i.communication_id.state in ('call', 'pending')):
-                comm = invoice.communication_id
-                object_ids = comm.object_ids
-                for line in invoice.invoice_line_ids:
-                    object_ids = object_ids.replace(
-                        str(line.id), '').replace(',,', '').strip(',')
-                if object_ids:
-                    # Refresh donation receipt
-                    config = self.env['account.invoice.line'].browse(
-                        [int(i) for i in object_ids.split(',')]
-                    ).get_thankyou_config()
-                    send_mode = config.get_inform_mode(comm.partner_id)
-                    comm.write({
-                        'config_id': config.id,
-                        'object_ids': object_ids,
-                        'send_mode': send_mode[0],
-                        'auto_send': send_mode[1],
-                        'need_call': config.need_call,
-                    })
-                    comm.refresh_text()
-                else:
-                    comm.unlink()
-        return super(AccountInvoice, self).write(vals)
-
-    @api.multi
-    def group_by_partner(self):
-        """ Returns a dict with {partner_id: invoices}"""
-        res = dict()
-        for partner in self.mapped('partner_id'):
-            res[partner.id] = self.filtered(
-                lambda i: i.partner_id == partner)
-        return OrderedDict(sorted(
-            res.items(), key=lambda t: sum(t[1].mapped('amount_total')),
-            reverse=True
-        ))
-
     @api.model
     def thankyou_summary_cron(self):
         """
@@ -136,3 +77,18 @@ class AccountInvoice(models.Model):
                     lambda l: l.event_id == event).generate_thank_you()
             if other_thank:
                 other_thank.generate_thank_you()
+
+    @api.multi
+    def _filter_invoice_to_thank(self):
+        """
+        Given a recordset of paid invoices, return only those that have
+        to be thanked.
+        :return: account.invoice recordset
+        """
+        return self.filtered(
+            lambda i: (not i.communication_id or i.communication_id.state in (
+                'call', 'pending')) and i.invoice_type != 'sponsorship' and
+            (not i.mapped('invoice_line_ids.contract_id') or (
+                i.invoice_type == 'gift' and i.origin !=
+                'Automatic birthday gift'))
+        )
