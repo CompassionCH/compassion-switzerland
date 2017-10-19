b9dfe260032d8683c6b3d521d61c80f2aae3f8f5
Emanuel Cino <ecino@compassion.ch>
2017-10-19 09:02:03 +0200

    CO-1185 FIX attachments of biennials not working

--- partner_communication_switzerland/models/partner_communication.py
+++ partner_communication_switzerland/models/partner_communication.py
@@ -258,6 +258,7 @@ class PartnerCommunication(models.Model):
                 attachments += attachment.copy({
                     'name': child.local_id + ' ' + child.last_photo_date +
                     '.jpg'})
+            self.env.invalidate_all()
             self.with_context(no_print=True).ir_attachment_ids = attachments
         else:
             self.ir_attachment_ids = False
