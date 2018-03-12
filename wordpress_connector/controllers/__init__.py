e9da0a119eba2667ffa6b5dd9f0063bc9c803004
Emanuel Cino <ecino@compassion.ch>
2018-03-12 13:44:20 +0100

    Add a test controller for mobile app-framework tests

--- wordpress_connector/controllers/__init__.py
+++ wordpress_connector/controllers/__init__.py
@@ -1,13 +1,11 @@
 # -*- coding: utf-8 -*-
 ##############################################################################
 #
-#    Copyright (C) 2016 Compassion CH (http://www.compassion.ch)
-#    Releasing children from poverty in Jesus' name
-#    @author: Philippe Heer <heerphilippe@msn.com>
+#    Copyright (C) 2018 Compassion CH (http://www.compassion.ch)
+#    @author: Emanuel Cino <ecino@compassion.ch>
 #
 #    The licence is in the file __manifest__.py
 #
 ##############################################################################
 
-from . import models
-from . import controllers
+import main
