d63cf56f88e28697bee40cf88be4590bd6ac1314
Sebastien Toth <popod@me.com>
2018-04-13 17:15:17 +0200

    CO-1598 Web page for Muskathlons events

--- muskathlon/controller/main.py
+++ muskathlon/controller/main.py
@@ -16,16 +16,36 @@ class MuskathlonWebsite(http.Controller):
     def list(self, **kw):
         events = http.request.env['crm.event.compassion']
         return http.request.render('muskathlon.list', {
-            'events': events.search([]).filtered(lambda x: x.muskathlon_event_id),
+            'events': events.search([]).filtered(
+                lambda x: x.muskathlon_event_id),
             # event.year = current_year !
         })
 
-    @http.route('/event/<model("crm.event.compassion"):event>/', auth='public', website=True)
+    @http.route('/event/<model("crm.event.compassion"):event>/',
+                auth='public', website=True)
     def details(self, event):
         return http.request.render('muskathlon.details', {
             'event': event
         })
 
+    @http.route('/event/<model("crm.event.compassion"):event>/participant'
+                '/<model("res.partner"):partner>/', auth='public',
+                website=True)
+    def participant_details(self, event, partner):
+        registration = event.muskathlon_registration_ids.filtered(
+            lambda item: item.partner_id.id == partner.id)
+
+        # if partner exist, but is not part of the muskathlon, return 404
+        if not registration:
+            return http.request.render('website.404')
+
+        return http.request.render('muskathlon.participant_details', {
+            'event': event,
+            'participant': partner,
+            'registration': registration
+        })
+
+    # snippet route for participants_list
     # @http.route('/event-participants/<int:event_id>/', type='json', auth='public', website=True)
     # def participants_list(self, event_id):
     #     participants = http.request.env['muskathlon.registration'].search([('event_id', '=', event_id)])
@@ -39,21 +59,3 @@ class MuskathlonWebsite(http.Controller):
     #     return http.request.render('muskathlon.media_list_template', {
     #         'posts': participants
     #     })
-
-    @http.route('/event/<model("crm.event.compassion"):event>/participant/<model("res.partner"):partner>/', auth='public', website=True)
-    def participant_details(self, event, partner):
-        muskathlon_report = http.request.env['muskathlon.report']
-
-        amount_win = int(sum(item.amount for item in muskathlon_report.search([]) if item.user_id.id == partner.id))
-        amount_win_percents = int(amount_win * 100 / 10000)
-
-        return http.request.render('muskathlon.participant_details', {
-            'event': event,
-            'participant': partner,
-            'amount_win': amount_win,
-            'amount_win_percents': amount_win_percents
-        })
-
-    @http.route('/contact', auth='public', website=True)
-    def contact(self):
-        return http.request.render('website.contactus', {})
