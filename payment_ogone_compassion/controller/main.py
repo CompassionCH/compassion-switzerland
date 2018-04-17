3f5f573f6ce6dd5b1fb02b5d480781b7a384172f
Nicolas Badoux <n.badoux@hotmail.com>
2018-04-17 14:52:31 +0200

    Fixing issues with unicode and urls

--- payment_ogone_compassion/controller/main.py
+++ payment_ogone_compassion/controller/main.py
@@ -13,25 +13,27 @@ import pprint
 
 from odoo import http
 from odoo.http import request
-from odoo.addons.payment.models.payment_acquirer import ValidationError
+from odoo.addons.payment_ogone.controllers.main import OgoneController
 
 _logger = logging.getLogger(__name__)
 
-class PaymentOgoneCompassionController(http.Controller):
-    _accept_url = '/payment/donation/test/accept'
-    _decline_url = '/payment/donation/test/decline'
-    _exception_url = '/payment/donation/test/exception'
-    _cancel_url = '/payment/donation/test/cancel'
+OgoneController._accept_url = '/payment/donation/test/accept'
+OgoneController._cancel_url = '/payment/donation/test/cancel'
+OgoneController._decline_url = '/payment/donation/test/decline'
+OgoneController._except_url = '/payment/donation/test/except'
+
+
+class OgoneController2(http.Controller):
     # TODO
     # need to skim
     # https://compassion.ch/fr/annulation-don
-    # https://compassion.ch/fr/confirmation-don
     @http.route([
         '/payment/donation/accept', '/payment/donation/test/accept/',
     ], type='http', auth='public', website=True)
     def ogone_form_feedback_accept(self, **post):
         """ Ogone contacts using GET, at least for accept """
-        _logger.info('Ogone: entering form_feedback with post data %s', pprint.pformat(post))  # debug
+        _logger.info('Ogone: entering form_feedback with post data %s',
+                     pprint.pformat(post))  # debug
         if request.env['payment.transaction'].sudo().form_feedback(post,
                                                                   'ogone'):
             http.request.env['payment.acquirer']\
