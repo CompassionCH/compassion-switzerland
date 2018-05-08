db040172c907adec75464bd53eafd7ec1136453e
Emanuel Cino <ecino@compassion.ch>
2018-05-08 11:34:35 +0200

    [FIX] Muskathlon bug, view and test fixes
    - Security rules
    - Improve views
    - Fix tests of wordpress_connector module

--- partner_compassion/models/ambassador_details.py
+++ partner_compassion/models/ambassador_details.py
@@ -8,20 +8,31 @@
 #
 ##############################################################################
 
-from odoo import models, fields
+from odoo import api, models, fields
 
 
 class AmbassadorDetails(models.Model):
     _name = "ambassador.details"
     _description = "Additional details about an ambassador"
+    _rec_name = "partner_id"
 
-    partner_id = fields.Many2one('res.partner')
+    partner_id = fields.Many2one(
+        'res.partner', required=True, ondelete='cascade')
     description = fields.Text(translate=True)
     quote = fields.Text(translate=True)
     picture_1 = fields.Binary(attachment=True)
+    filename_1 = fields.Char(compute='_compute_filenames')
     picture_2 = fields.Binary(attachment=True)
+    filename_2 = fields.Char(compute='_compute_filenames')
     thank_you_quote = fields.Html(
         help='Used in thank you letters for donations linked to an event '
              'and to this partner.',
     )
     mail_copy_when_donation = fields.Boolean()
+
+    @api.multi
+    def _compute_filenames(self):
+        for details in self:
+            partner_name = details.display_name
+            details.filename_1 = partner_name + '-1.jpg'
+            details.filename_2 = partner_name + '-2.jpg'
