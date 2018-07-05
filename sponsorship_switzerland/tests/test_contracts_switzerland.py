fa7016f9129e71488d1307bf3f128184aae8806d
nbornand <nicolas74@gmail.com>
2018-07-05 13:37:18 +0200

    CO-1644 add test for sponsorship termination

--- sponsorship_switzerland/tests/test_contracts_switzerland.py
+++ sponsorship_switzerland/tests/test_contracts_switzerland.py
@@ -24,20 +24,30 @@ class TestContractsSwitzerland(BaseSponsorshipTest):
 
     @mock.patch(time_path)
     def test_on_change_group_id__recomputes_next_invoice_date(self, time_mock):
+        sponsorship = self._create_sponsorship()
+        time_mock.now.return_value = datetime.date(2015, 2, 5)
+
+        sponsorship.on_change_group_id()
+
+        self.assertEqual(sponsorship.next_invoice_date, '2015-02-01')
+
+    def test_sponsorship_termination(self):
+        sponsorship = self._create_sponsorship(contract_type='S')
+
+        self.assertEqual(self.michel.number_sponsorships, 0)
+        sponsorship.contract_terminated()
+        self.assertEqual(self.michel.number_sponsorships, 1)
+
+    def _create_sponsorship(self, contract_type='SC'):
         child = self.create_child('IO06790211')
         partner_id = self.michel.id
         group = self.create_group({'partner_id': partner_id})
-        sponsorship = self.create_contract(
+        return self.create_contract(
             {
-                'type': 'SC',
+                'type': contract_type,
                 'child_id': child.id,
                 'group_id': group.id,
                 'partner_id': partner_id,
             },
             [{'amount': 50.0}]
         )
-        time_mock.now.return_value = datetime.date(2015, 2, 5)
-
-        sponsorship.on_change_group_id()
-
-        self.assertEqual(sponsorship.next_invoice_date, '2015-02-01')
