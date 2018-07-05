b863c0deea3dded8f92915e3f57938cc9686ecd9
nbornand <nicolas74@gmail.com>
2018-07-05 08:55:10 +0200

    CO-1644 uncomment test

--- sponsorship_switzerland/tests/__init__.py
+++ sponsorship_switzerland/tests/__init__.py
@@ -1,14 +1,14 @@
 # -*- coding: utf-8 -*-
 ##############################################################################
 #
-#    Copyright (C) 2015 Compassion CH (http://www.compassion.ch)
+#    Copyright (C) 2018 Compassion CH (http://www.compassion.ch)
 #    Releasing children from poverty in Jesus' name
-#    @author: Albert SHENOUDA <albert.shenouda@efrei.net>
+#    @author: Nicolas BORNAND <nicolas74@gmail.com>
 #
 #    The licence is in the file __manifest__.py
 #
 ##############################################################################
 
-# from . import test_completion_rules_switzerland
+from . import test_completion_rules_switzerland
 from . import test_contract_group_switzerland
 
