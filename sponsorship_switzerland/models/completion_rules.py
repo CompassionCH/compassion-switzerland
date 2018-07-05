c15f6947936217b6f4ea43ef733fc0c5aea8f48f
nbornand <nbornand@users.noreply.github.com>
2018-07-05 16:15:52 +0200

    Co 1644 improve coverage on compassion switzerland (#539)
    CO-1644 add tests for sponsorship_compassion
    
    * add test rule auto complete; lookup by partner reference
    
    * add test rule auto complete; for bvrs
    
    * add test rule auto complete; for parner lookup by name
    
    * add test rule auto complete; for LSV
    
    * add tests for contract groups
    
    * add test for contracts

--- sponsorship_switzerland/models/completion_rules.py
+++ sponsorship_switzerland/models/completion_rules.py
@@ -137,49 +137,57 @@ class StatementCompletionRule(models.Model):
 
     def get_from_lsv_dd(self, stmts_vals, st_line):
         """ If line is a LSV or DD credit, change the account to 1098. """
+        def get_postfinance_id():
+            return self.env['res.partner'].search([
+                ('name', '=', 'Postfinance SA')
+            ], limit=1).commercial_partner_id.id
+
+        def fetch_account_id_by_code(code):
+            return self.env['account.account'] \
+                .search([('code', '=', code)], limit=1).id
+
         label = st_line['name'].replace('\n', ' ') if st_line[
             'name'] != '/' else st_line['ref'].replace('\n', ' ')
         lsv_dd_strings = [u'BULLETIN DE VERSEMENT ORANGE',
                           u'ORDRE DEBIT DIRECT',
                           u'CRÉDIT GROUPÉ BVR',
                           u'Crèdit LSV']
-        is_lsv_dd = False
-        res = {}
-        account_id = False
+
         if st_line['amount'] >= 0:
             if u'KREDITKARTEN' in label:
-                account_id = self.env['account.account'].search(
-                    [('code', '=', '2000')], limit=1).id
-                res['partner_id'] = self.env['res.partner'].search([
-                    ('name', '=', 'Postfinance SA')
-                ], limit=1).commercial_partner_id.id
+                return {
+                    'account_id': fetch_account_id_by_code('2000'),
+                    'partner_id': get_postfinance_id()
+                }
             elif u'CRÉDIT TRANSACTIONS E-PAYMENT POSTFINANCE CARD' in label:
-                account_id = self.env['account.account'].search(
-                    [('code', '=', '1015')], limit=1).id
-                res['partner_id'] = self.env['res.partner'].search([
-                    ('name', '=', 'Postfinance SA')
-                ], limit=1).commercial_partner_id.id
+                return {
+                    'account_id': fetch_account_id_by_code('1015'),
+                    'partner_id': get_postfinance_id()
+                }
             else:
-                for credit_string in lsv_dd_strings:
-                    is_lsv_dd = is_lsv_dd or credit_string in label
-            if is_lsv_dd:
-                account_id = self.env['account.account'].search(
-                    [('code', '=', '1098')], limit=1).id
-            if account_id:
-                res['account_id'] = account_id
-
-        return res
+                is_lsv_dd = any(s in label for s in lsv_dd_strings)
+                if is_lsv_dd:
+                    account_id = fetch_account_id_by_code('1098')
+                    if account_id:
+                        return {
+                            'account_id': fetch_account_id_by_code('1098')
+                        }
+        return {}
 
     def get_sponsor_name(self, st_vals, st_line):
         res = {}
         name = st_line['name']
+        wire_transfer_pattern = u"VIREMENT DU COMPTE "
         patterns_lookup = [u" EXPÉDITEUR: ", u" DONNEUR D'ORDRE: ",
-                           u"VIREMENT DU COMPTE "]
+                           wire_transfer_pattern]
+
+        def search_partner(criteria):
+            return self.env['res.partner'].search(criteria)
 
         for pattern in patterns_lookup:
             if pattern in name:
                 sender_info = name.split(pattern)[1]
-                if pattern == patterns_lookup[2]:
+                if pattern == wire_transfer_pattern:
                     # First the account of partner, then the name
                     # (lastname is at first)
                     name_guess = sender_info.split(" ")[1:3]
@@ -188,19 +196,19 @@ class StatementCompletionRule(models.Model):
                     # Guess the name with the two first words (following words
                     # could be part of the address (firstname is at first)
                     name_guess = sender_info.split(" ")[:2]
-                partner = self.env['res.partner'].search([
+                partner = search_partner([
                     ('firstname', 'ilike', name_guess[0]),
                     ('lastname', '=ilike', name_guess[1])
-                ])
+                ]) if len(name_guess) >= 2 else []
                 if len(partner) > 1:
                     # Try to do exact search on firstname
-                    partner = self.env['res.partner'].search([
+                    partner = search_partner([
                         ('firstname', '=ilike', name_guess[0]),
                         ('lastname', '=ilike', name_guess[1])
                     ])
                 if not partner:
                     # Try to find a company
-                    partner = self.env['res.partner'].search([
+                    partner = search_partner([
                         ('name', 'ilike', name_guess[0]),
                         ('is_company', '=', True)
                     ])
