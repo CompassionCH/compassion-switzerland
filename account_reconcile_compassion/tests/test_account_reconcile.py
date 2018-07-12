65ac2d1b1ced30ac02b4b464908157374bc5dd21
Civiliste Compassion <gigon.quentin@gmail.com>
2018-07-12 17:07:23 +0200

    CO-1641 values are created, need to test scenarios now

---
+++ account_reconcile_compassion/tests/test_account_reconcile.py
@@ -0,0 +1,94 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    Copyright (C) 2018 Compassion CH (http://www.compassion.ch)
+#    @author: Quentin Gigon <gigon.quentin@gmail.com>
+#
+#    The licence is in the file __manifest__.py
+#
+##############################################################################
+
+from odoo import models, fields, api
+
+from odoo.addons.sponsorship_compassion.tests.test_sponsorship_compassion \
+    import BaseSponsorshipTest
+
+
+class TestAccountReconcile(BaseSponsorshipTest):
+
+    def setUp(self):
+        super(TestAccountReconcile, self).setUp()
+
+        self.t_child = self.create_child('TT123456789')
+        self.t_partner = self.env.ref('base.res_partner_address_31')
+        t_group = self.create_group({'partner_id': self.t_partner.id})
+        self.t_sponsorship = self.create_contract({
+            'partner_id': self.t_partner.id,
+            'group_id': t_group.id,
+            'child_id': self.t_child.id,
+        },
+            [{'amount': 50.0}])
+
+    def test_account_reconcile(self):
+
+        journal = self.env['account.journal'].create({
+            'name': 'test_journal',
+            'code': '1050',
+            'type': 'sale'
+        })
+        self.assertTrue(journal)
+        print(self.env.context.get('default_journal_id'))
+
+        account = self.env['account.account'].create({
+            'name': 'TestAccount',
+            'currency_id': self.env.ref('base.CHF').id,
+            'code': '1051',
+            'reconcile': True,
+            'user_type_id': self.t_partner.id
+        })
+        self.assertTrue(account)
+
+        bank_statement = self.env['account.bank.statement'].create({
+            'date': fields.Date.today(),
+            'state': 'open',
+            'journal_id': journal.id,
+        })
+        self.assertTrue(bank_statement)
+
+        #self.assertEquals(bank_statement._default_name(), 'test_journal')
+
+        bank_statement_line = self.env['account.bank.statement.line'].create({
+            'name': 'TestBankLine',
+            'date': fields.Date.today(),
+            'amount': 50,
+            'journal_id': journal.id,
+            'account_id': account.id,
+            'statement_id': bank_statement.id
+        })
+        self.assertTrue(bank_statement_line)
+        self.env['account.bank.statement.line'].write
+
+        move = self.env['account.move'].create({
+            'name': 'test_acc_move',
+            'date': fields.Date.today(),
+            'journal_id': journal.id,
+            'state': 'draft',
+
+        })
+        self.assertTrue(move)
+
+        account_move_line = self.env['account.move.line'].create({
+            'name': 'test_move_line',
+            'account_id': account.id,
+            'move_id': move.id,
+            'date_maturity': '2018-12-12',
+            'currency_id': self.env.ref('base.CHF').id
+        })
+        self.assertTrue(account_move_line)
+
+        acc_partial_rec = self.env['account.partial.reconcile'].create({
+            'debit_move_id': account_move_line.id,
+            'credit_move_id': account_move_line.id
+        })
+        self.assertTrue(acc_partial_rec)
+
