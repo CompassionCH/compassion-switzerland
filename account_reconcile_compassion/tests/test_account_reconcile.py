8aa9764eed355eb194646bcbe059098b0e583d42
Civiliste Compassion <gigon.quentin@gmail.com>
2018-07-13 15:41:28 +0200

    added some tests

--- account_reconcile_compassion/tests/test_account_reconcile.py
+++ account_reconcile_compassion/tests/test_account_reconcile.py
@@ -8,10 +8,10 @@
 #
 ##############################################################################
 
-from odoo import models, fields, api
-
+from odoo import fields
 from odoo.addons.sponsorship_compassion.tests.test_sponsorship_compassion \
     import BaseSponsorshipTest
+from datetime import date
 
 
 class TestAccountReconcile(BaseSponsorshipTest):
@@ -29,6 +29,12 @@ class TestAccountReconcile(BaseSponsorshipTest):
         },
             [{'amount': 50.0}])
 
+        self.company = self.env['res.company'].create({
+            'name': 'Test Company',
+            'partner_id': self.t_partner.id,
+            'currency_id': self.env.ref('base.USD').id
+        })
+
         self.journal = self.env['account.journal'].search([
             ('code', '=', 'CCP')
         ])
@@ -36,6 +42,7 @@ class TestAccountReconcile(BaseSponsorshipTest):
     def test_account_reconcile(self):
 
         self.assertTrue(self.journal)
+        self.assertTrue(self.company)
 
         account = self.env['account.account'].create({
             'name': 'TestAccount',
@@ -46,12 +53,33 @@ class TestAccountReconcile(BaseSponsorshipTest):
         })
         self.assertTrue(account)
 
-        company = self.env['res.company'].create({
-            'name': 'Test Company',
-            'partner_id': self.t_partner.id,
+        move = self.env['account.move'].create({
+            'name': 'test_acc_move',
+            'date': fields.Date.today(),
+            'journal_id': self.journal.id,
+            'state': 'draft',
+            'company_id': self.company.id,
+            'ref': 'test_ref'
+        })
+        self.assertTrue(move)
+
+        account_move_line = self.env['account.move.line'].create({
+            'name': 'test_move_line',
+            'account_id': account.id,
+            'move_id': move.id,
+            'date_maturity': '2018-12-12',
             'currency_id': self.env.ref('base.CHF').id
         })
-        self.assertTrue(company)
+        self.assertTrue(account_move_line)
+
+        account_move_line_today = self.env['account.move.line'].create({
+            'name': 'test_move_line',
+            'account_id': account.id,
+            'move_id': move.id,
+            'date_maturity': fields.Date.today(),
+            'currency_id': self.env.ref('base.CHF').id
+        })
+        self.assertTrue(account_move_line_today)
 
         bank_statement = self.env['account.bank.statement'].create({
             'date': fields.Date.today(),
@@ -66,9 +94,18 @@ class TestAccountReconcile(BaseSponsorshipTest):
             'amount': 50,
             'journal_id': self.journal.id,
             'account_id': account.id,
-            'statement_id': bank_statement.id
+            'statement_id': bank_statement.id,
+            'ref': 'test_ref',
+            'currency_id': account.currency_id.id
         })
         self.assertTrue(bank_statement_line)
+
+        # should be 12 - 6 * 12 = 72
+        self.assertEquals(bank_statement_line._sort_move_line(
+            account_move_line), 72)
+        # should be 1
+        self.assertEquals(bank_statement_line._sort_move_line(
+            account_move_line_today), 1)
 
         # test linking partner to bank when writing to
         # account.bank.statement.line
@@ -86,23 +123,6 @@ class TestAccountReconcile(BaseSponsorshipTest):
         self.assertEquals(
             len(bank_statement_line.get_move_lines_for_reconciliation(
                 limit=12)), 12)
-
-        move = self.env['account.move'].create({
-            'name': 'test_acc_move',
-            'date': fields.Date.today(),
-            'journal_id': self.journal.id,
-            'state': 'draft'
-        })
-        self.assertTrue(move)
-
-        account_move_line = self.env['account.move.line'].create({
-            'name': 'test_move_line',
-            'account_id': account.id,
-            'move_id': move.id,
-            'date_maturity': '2018-12-12',
-            'currency_id': self.env.ref('base.CHF').id
-        })
-        self.assertTrue(account_move_line)
 
         acc_partial_rec = self.env['account.partial.reconcile'].create({
             'debit_move_id': account_move_line.id,
