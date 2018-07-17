413cf565449763f2f54dcf1c2aee77e4f88d1963
Emanuel Cino <ecino@compassion.ch>
2018-07-17 09:12:35 +0200

    CO-1903 [IMP] Muskathlon flyer order

--- muskathlon/forms/order_material_form.py
+++ muskathlon/forms/order_material_form.py
@@ -7,9 +7,16 @@
 #    The licence is in the file __manifest__.py
 #
 ##############################################################################
+import logging
 from odoo import models, fields, tools, _
 
 testing = tools.config.get('test_enable')
+_logger = logging.getLogger(__name__)
+
+try:
+    import magic
+except ImportError:
+    _logger.warning('Please install magic in order to use Muskathlon module')
 
 
 if not testing:
@@ -68,11 +75,14 @@ if not testing:
             """ Dismiss any pending status message, to avoid multiple
             messages when multiple forms are present on same page.
             """
+            super(OrderMaterialForm, self).form_before_create_or_update(
+                values, extra_values)
             self.o_request.website.get_status_message()
             staff_id = self.env['staff.notification.settings']\
-                .sudo().get_param('muskathlon_lead_notify_id')
+                .sudo().get_param('muskathlon_order_notify_id')
             values.update({
-                'name': "Muskathlon material order",
+                'name': "Muskathlon material order - {}".format(
+                    self.partner_id.name),
                 'description': "Number of flyers wanted: " + extra_values[
                     'flyer_number'],
                 'user_id': staff_id,
@@ -83,13 +93,38 @@ if not testing:
         def _form_create(self, values):
             """ Run as Muskathlon user to authorize lead creation. """
             uid = self.env.ref('muskathlon.user_muskathlon_portal').id
-            super(OrderMaterialForm, self.sudo(uid))._form_create(values)
+            self.main_object = self.form_model.sudo(uid).create(values.copy())
+
+        def form_after_create_or_update(self, values, extra_values):
+            super(OrderMaterialForm, self).form_after_create_or_update(
+                values, extra_values)
+            # Update contact fields on lead
+            self.main_object._onchange_partner_id()
 
     class OrderMaterialFormFlyer(models.AbstractModel):
         _name = 'cms.form.order.material'
         _inherit = 'cms.form.order.material.mixin'
 
         form_id = fields.Char(default='order_material')
+
+        def form_after_create_or_update(self, values, extra_values):
+            super(OrderMaterialFormFlyer, self).form_after_create_or_update(
+                values, extra_values)
+            # Attach ambassador picture for flyer creation
+            ftype = 'jpg'
+            partner = self.main_object.partner_id
+            mimetype = magic.from_buffer(partner.image.decode('base64'), True)
+            if mimetype and '/' in mimetype:
+                ftype = mimetype.split('/')[-1]
+            filename = '{}.{}'.format(partner.name, ftype)
+            uid = self.env.ref('muskathlon.user_muskathlon_portal').id
+            self.env['ir.attachment'].sudo(uid).create({
+                'res_model': 'crm.lead',
+                'res_id': self.main_object.id,
+                'datas': partner.image,
+                'datas_fname': filename,
+                'name': filename
+            })
 
     class OrderMaterialFormChildpack(models.AbstractModel):
         _name = 'cms.form.order.muskathlon.childpack'
@@ -102,7 +137,8 @@ if not testing:
             super(OrderMaterialFormChildpack,
                   self).form_before_create_or_update(values, extra_values)
             values.update({
-                'name': "Muskathlon childpack order",
+                'name': "Muskathlon childpack order - {}".format(
+                    self.partner_id.name),
                 'description': "Number of childpacks wanted: " + extra_values[
                     'flyer_number'],
             })
