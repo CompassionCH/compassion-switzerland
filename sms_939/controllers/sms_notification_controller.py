1ba5885e599f00c80f43bebba77792a3b864f4fb
Emanuel Cino <ecino@compassion.ch>
2018-07-25 09:13:40 +0200

    log received SMS request in console

--- sms_939/controllers/sms_notification_controller.py
+++ sms_939/controllers/sms_notification_controller.py
@@ -8,9 +8,13 @@
 #    The licence is in the file __manifest__.py
 #
 ##############################################################################
+import logging
+import json
 
 from odoo import http
 from odoo.http import request
+
+_logger = logging.getLogger(__name__)
 
 
 class RestController(http.Controller):
@@ -18,6 +22,8 @@ class RestController(http.Controller):
     @http.route('/sms/mnc/', type='http', auth='public', methods=['POST'],
                 csrf=False)
     def sms_notification(self, **parameters):
+        _logger.info("SMS Request received : {}".format(
+            json.dumps(parameters)))
         sms = request.env['sms.notification'].sudo().create({
             'instance': parameters.get('instance'),
             'sender': parameters.get('sender'),
