982372698a5ee1a756accc8760741852b8cca721
nbornand <nicolas74@gmail.com>
2018-08-10 10:41:57 +0200

    CO-1969 add migration script

--- partner_compassion/__manifest__.py
+++ partner_compassion/__manifest__.py
@@ -30,7 +30,7 @@
 # pylint: disable=C8101
 {
     'name': 'Upgrade Partners for Compassion Suisse',
-    'version': '10.0.1.2.0',
+    'version': '10.0.1.5.0',
     'category': 'Partner',
     'author': 'Compassion CH',
     'license': 'AGPL-3',
