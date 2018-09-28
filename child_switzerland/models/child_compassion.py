a004abd76af602192704cf4d01d9daf3903d6477
Emanuel Cino <ecino@compassion.ch>
2018-09-28 15:04:56 +0200

    Remove unused code

--- child_switzerland/models/child_compassion.py
+++ child_switzerland/models/child_compassion.py
@@ -8,7 +8,7 @@
 #    The licence is in the file __manifest__.py
 #
 ##############################################################################
-from odoo import models, fields, api
+from odoo import models, fields
 
 
 class CompassionChild(models.Model):
@@ -17,33 +17,3 @@ class CompassionChild(models.Model):
     desc_fr = fields.Text('French description', readonly=True)
     desc_de = fields.Text('German description', readonly=True)
     desc_it = fields.Text('Italian description', readonly=True)
-
-    @api.model
-    def correct_old_children(self):
-        old_children = self.search([('global_id', '=', False)]).filtered(
-            lambda c: len(c.local_id) < 11)
-        for child in old_children:
-            if child.code:
-                child.local_id = child.code[0:2] + '0' + \
-                    child.code[2:5] + '0' + child.code[5:]
-        return True
-
-    @api.model
-    def find_missing_global_id(self):
-        missing_gids = self.search([('global_id', '=', False)])
-        global_search = self.env['compassion.childpool.search'].create({
-            'take': 1,
-        })
-        cpt = 0
-        for child in missing_gids:
-            try:
-                global_search.local_id = child.local_id
-                global_search.do_search()
-                if global_search.global_child_ids:
-                    child.global_id = global_search.global_child_ids.global_id
-                # Commit at each fix
-                self.env.cr.commit()    # pylint: disable=invalid-commit
-            except:
-                self.env.invalidate_all()
-            finally:
-                cpt += 1
