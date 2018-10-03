6765acff2a4e3d9a48ed06b1dc831ac69611845c
Emanuel Cino <ecino@compassion.ch>
2018-10-03 15:34:23 +0200

    CO-2046 Add opt_out date in res_partner

---
+++ mass_mailing_switzerland/models/res_partner.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    Copyright (C) 2018 Compassion CH (http://www.compassion.ch)
+#    Releasing children from poverty in Jesus' name
+#    @author: Emanuel Cino <ecino@compassion.ch>
+#
+#    The licence is in the file __manifest__.py
+#
+##############################################################################
+
+from odoo import models, api, fields
+
+
+class ResPartner(models.Model):
+    _inherit = "res.partner"
+
+    date_opt_out = fields.Date()
+
+    @api.model
+    def create(self, vals):
+        if vals.get('opt_out'):
+            vals['date_opt_out'] = fields.Date.today()
+        return super(ResPartner, self).create(vals)
+
+    @api.multi
+    def write(self, vals):
+        if vals.get('opt_out'):
+            vals['date_opt_out'] = fields.Date.today()
+        elif 'opt_out' in vals:
+            vals['date_opt_out'] = False
+        return super(ResPartner, self).write(vals)
