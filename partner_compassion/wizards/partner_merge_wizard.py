5f064f73dc7917b2e9254348f63fcb1900bf9e6a
Emanuel Cino <ecino@compassion.ch>
2018-11-16 15:00:08 +0100

    CO-2142 Save e-mail to secondary contact when merge partner

--- partner_compassion/wizards/partner_merge_wizard.py
+++ partner_compassion/wizards/partner_merge_wizard.py
@@ -19,9 +19,10 @@ class PartnerMergeWizard(models.TransientModel):
     @api.multi
     def action_merge(self):
         """
-        Allow anybody to perform the merge of partners
-        Prevent geoengine bugs
-        Prevent to merge sponsors
+        - Allow anybody to perform the merge of partners
+        - Prevent geoengine bugs
+        - Prevent to merge sponsors
+        - Save other e-mail addresses in linked partners
         """
         removing = self.partner_ids - self.dst_partner_id
         geo_point = self.dst_partner_id.geo_point
@@ -37,6 +38,15 @@ class PartnerMergeWizard(models.TransientModel):
                 "Please first modify the sponsorship and don't forget "
                 "to send new labels to them."
             ))
+        old_emails = removing.filtered('email').mapped('email')
+        new_email = self.dst_partner_id.email
+        for email in old_emails:
+            if new_email and email != new_email:
+                self.dst_partner_id.copy({
+                    'contact_id': self.dst_partner_id.id,
+                    'email': email,
+                    'type': 'email_alias'
+                })
         res = super(PartnerMergeWizard, self.sudo()).action_merge()
         self.dst_partner_id.geo_point = geo_point
         return res
