19bac183a333f911f3ef7a5feae861f660e391e7
Emanuel Cino <ecino@compassion.ch>
2018-12-11 15:28:58 +0100

    CO-2246 Map Write&Pray communications to XML records

--- partner_communication_switzerland/models/contracts.py
+++ partner_communication_switzerland/models/contracts.py
@@ -462,6 +462,11 @@ class RecurringContract(models.Model):
         ]).unlink()
         # This prevents sending welcome e-mail if it's already active
         self.write({'sds_state': 'active'})
+        # Send new dossier for write&pray sponsorships
+        self.filtered(lambda s: s.type == 'SC').send_communication(
+            self.env.ref('partner_communication_switzerland'
+                         '.sponsorship_dossier_wrpr')
+        )
         return super(RecurringContract, self).contract_active()
 
     @api.multi
