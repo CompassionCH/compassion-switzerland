2ee666f700635fa2ee0ee4112e70e3f23cc159b2
Emanuel Cino <ecino@compassion.ch>
2019-01-10 12:01:55 +0100

    CO-2282 FIX old muskathlon registrations published on website

--- website_event_compassion/__manifest__.py
+++ website_event_compassion/__manifest__.py
@@ -30,7 +30,7 @@
 # pylint: disable=C8101
 {
     'name': 'Compassion Events Website',
-    'version': '10.0.1.4.2',
+    'version': '10.0.1.4.4',
     'category': 'Other',
     'author': 'Compassion CH',
     'license': 'AGPL-3',
