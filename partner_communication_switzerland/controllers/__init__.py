aafb0fab18a727a0756271bf5868ef3990c2961c
Emanuel Cino <ecino@compassion.ch>
2019-02-19 09:41:15 +0100

    CO-2147 Use the download link for B2S letters sent by e-mail
    - Never attach pdf when letter is sent by e-mail
    - Mark letter as opened and read when the link is accessed
    -

--- partner_communication_switzerland/controllers/__init__.py
+++ partner_communication_switzerland/controllers/__init__.py
@@ -1,14 +1,11 @@
 # -*- coding: utf-8 -*-
 ##############################################################################
 #
-#    Copyright (C) 2016 Compassion CH (http://www.compassion.ch)
+#    Copyright (C) 2015 Compassion CH (http://www.compassion.ch)
 #    Releasing children from poverty in Jesus' name
 #    @author: Emanuel Cino <ecino@compassion.ch>
 #
 #    The licence is in the file __manifest__.py
 #
 ##############################################################################
-
-from . import models
-from . import wizards
-from . import controllers
+from . import b2s_image
