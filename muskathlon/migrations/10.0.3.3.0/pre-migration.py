e1d766bcbf1aa3776fe26756d1d587e118323186
Emanuel Cino <ecino@compassion.ch>
2019-05-02 11:03:28 +0200

    CO-2493 FIX muskathlon medical survey migration

--- muskathlon/migrations/10.0.3.3.0/pre-migration.py
+++ muskathlon/migrations/10.0.3.3.0/pre-migration.py
@@ -19,7 +19,7 @@ def migrate(env, version):
         return
 
     for registration in env['event.registration'].search([
-            ('survey_id', '=', False),
+            ('medical_survey_id', '=', False),
             ('event_id.medical_survey_id', '!=', False)]):
         survey = registration.event_id.medical_survey_id
         medical_survey_id = env['survey.user_input'].search([
