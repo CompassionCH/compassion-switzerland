6e52dba6347c616bed1f1ae1976ba6249ff04c1f
erp-q <gigon.quentin@gmail.com>
2019-05-16 09:48:02 +0200

    CO-2527 pdf is now correctly generated

--- muskathlon/models/survey_user_input.py
+++ muskathlon/models/survey_user_input.py
@@ -7,12 +7,15 @@
 #    The licence is in the file __manifest__.py
 #
 ##############################################################################
-
+import base64
 from odoo import models, api
+from odoo.tools import safe_eval
 
 
 class SurveyUserInput(models.Model):
-    _inherit = 'survey.user_input'
+    _name = 'survey.user_input'
+
+    _inherit = ['survey.user_input', 'mail.template']
 
     @api.multi
     def write(self, vals):
@@ -31,20 +34,45 @@ class SurveyUserInput(models.Model):
                     'muskathlon.stage_fill_profile').id)
             ])
 
-            for registration in registrations:
-                medical_survey_id = self.env['survey.user_input'].search([
-                    ('partner_id', '=', registration.partner_id_id),
-                    ('survey_id', '=',
-                     registration.event_id.medical_survey_id.id)
-                ], limit=1)
+            # if conditions to check that the task is the correct one,
+            # same as survey in event definition
+            registrations.write({
+                'completed_task_ids': [
+                    (4, self.env.ref(
+                        'muskathlon.task_medical').id)
+                ]
+            })
 
-                # if conditions to check that the task is the correct one,
-                # same as survey in event definition
-                registration.write({
-                    'completed_task_ids': [
-                        (4, self.env.ref(
-                            'muskathlon.task_medical').id)
-                    ],
-                    'medical_survey_id': medical_survey_id.id
-                })
+            # here we need to send a mail to the muskathlon doctor
+            icp_obj = self.env['ir.config_parameter']
+
+            muskathlon_doctor_email = safe_eval(
+                icp_obj.get_param('muskathlon_doctor_email')
+            )
+
+            # send mail only if there is a doctor email address
+            if muskathlon_doctor_email:
+
+                # generate pdf from report
+                pdf = base64.encodestring(
+                    self.env['report'].sudo().get_pdf(self.ids, 'muskathlon.muskathlon_medical_survey'))
+
+                # save pdf as attachment
+                ATTACHMENT_NAME = "Muskathlon Medical Survey"
+                attachment_data = {
+                    'name': ATTACHMENT_NAME,
+                    'type': 'binary',
+                    'datas': pdf,
+                    'datas_fname': ATTACHMENT_NAME + '.pdf',
+                    'store_fname': ATTACHMENT_NAME,
+                    'res_model': self._name,
+                    'res_id': self.id,
+                    'mimetype': 'application/x-pdf'
+                }
+
+                template = self.env.ref(
+                    "muskathlon.muskathlon_medical_survey_to_doctor_template")
+                template.with_context(email_to=muskathlon_doctor_email) \
+                    .send_mail(self.id, force_send=True, email_values=attachment_data)
+
         return res
