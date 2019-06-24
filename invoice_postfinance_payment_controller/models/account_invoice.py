536a7d4fc8b4e91e34c8f854d012fe94a44a6c9c
Emanuel Cino <ecino@compassion.ch>
2019-06-24 14:34:10 +0200

    CO-2648 CO-2650 Add wizard to send invoice for online payment

---
+++ invoice_postfinance_payment_controller/models/account_invoice.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    Copyright (C) 2019 Compassion CH (http://www.compassion.ch)
+#    @author: Emanuel Cino <ecino@compassion.ch>
+#
+#    The licence is in the file __manifest__.py
+#
+##############################################################################
+
+from odoo import models, fields
+
+
+class AccountInvoice(models.Model):
+    _inherit = "account.invoice"
+
+    accept_url = fields.Char(
+        help='Stores the redirection where the partner should go after the '
+        'transaction is completed.'
+    )
+    decline_url = fields.Char(
+        help='Stores the redirection where the partner should go after the '
+        'transaction is cancelled.'
+    )
