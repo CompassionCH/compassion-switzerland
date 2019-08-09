afc2a518343e73f69c956668084b096b2202ac6a
samybucher <52907842+samybucher@users.noreply.github.com>
2019-08-09 11:19:42 +0200

    CO-2685 (#921)
    * CO-2685
    creation of a link between thank you letters and gift. A wizard take the csv file and fill the fields between the objects.
    be careful, translation are missing and must be done in the production side
    
    * CO-2685 adding a "return=true" to indicate that everything went smoothly and a little improvement for the decoding of the csv
    
    * correction of the return who hasn't the good indentation

--- sponsorship_switzerland/models/gift_compassion.py
+++ sponsorship_switzerland/models/gift_compassion.py
@@ -4,6 +4,7 @@
 #    Copyright (C) 2018 Compassion CH (http://www.compassion.ch)
 #    Releasing children from poverty in Jesus' name
 #    @author: Emanuel Cino <ecino@compassion.ch>
+#    modification: Samy Bucher <samy.bucher@outlook.com>
 #
 #    The licence is in the file __manifest__.py
 #
@@ -11,12 +12,15 @@
 
 from datetime import date
 from dateutil.relativedelta import relativedelta
-
-from odoo import api, models, fields
+from odoo import api
+from odoo import models
+from odoo import fields
 
 
 class Gift(models.Model):
     _inherit = 'sponsorship.gift'
+
+    letter_id = fields.Many2one('correspondence', 'Thank you letter')
 
     @api.model
     def process_gifts_cron(self):
