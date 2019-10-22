64f86ab6d0ae9080399b14b396cfea4d9177021a
Emanuel Cino <ecino@compassion.ch>
2019-10-22 15:35:07 +0200

    CO-2687 Some further adaptations

--- partner_communication_switzerland/models/thankyou_config.py
+++ partner_communication_switzerland/models/thankyou_config.py
@@ -15,10 +15,10 @@ class ThankYouConfig(models.Model):
     _inherit = 'thankyou.config'
 
     @api.multi
-    def for_donation_amount(self, total_amount):
+    def for_donation(self, invoice_lines):
         # Special case for legacy donation : always treat as large donation
-        if 'legacy' in self.with_context(lang='en_US').mapped(
+        if 'legacy' in invoice_lines.with_context(lang='en_US').mapped(
                 'product_id.name'):
             # Will return the largest donation configuration
             return self.sorted()[-1]
-        return super(ThankYouConfig, self).for_donation_amount(total_amount)
+        return super(ThankYouConfig, self).for_donation(invoice_lines)
