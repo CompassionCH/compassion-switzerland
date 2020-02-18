3a9fc15df6cbab8b560379a094e1bb94b0c16e2e
Sylvain Losey <sylvainlosey@gmail.com>
2020-02-18 17:09:28 +0100

    Black

--- mis_builder_unpaid_invoice/models/mis_unpaid_invoice.py
+++ mis_builder_unpaid_invoice/models/mis_unpaid_invoice.py
@@ -9,47 +9,28 @@ from odoo import api, fields, models, tools
 
 class MisUnpaidInvoice(models.Model):
 
-    _name = 'mis.unpaid.invoice'
-    _description = 'MIS unpaid invoice'
+    _name = "mis.unpaid.invoice"
+    _description = "MIS unpaid invoice"
     _auto = False
 
     line_type = fields.Char()
     name = fields.Char()
-    account_id = fields.Many2one(
-        comodel_name='account.account',
-        string='Account',
-    )
-    invoice_id = fields.Many2one(
-        'account.invoice',
-        string='Invoice',
-    )
-    journal_id = fields.Many2one(
-        'account.journal',
-        string='Journal',
-    )
-    move_id = fields.Many2one(
-        'account.move',
-        string='Accounting move',
-    )
+    account_id = fields.Many2one(comodel_name="account.account", string="Account",)
+    invoice_id = fields.Many2one("account.invoice", string="Invoice",)
+    journal_id = fields.Many2one("account.journal", string="Journal",)
+    move_id = fields.Many2one("account.move", string="Accounting move",)
     analytic_account_id = fields.Many2one(
-        'account.analytic.account',
-        string='Analytic account',
+        "account.analytic.account", string="Analytic account",
     )
-    product_id = fields.Many2one(
-        'product.product',
-        string='Product',
-    )
-    company_id = fields.Many2one(
-        comodel_name='res.company',
-        string='Company',
-    )
+    product_id = fields.Many2one("product.product", string="Product",)
+    company_id = fields.Many2one(comodel_name="res.company", string="Company",)
     credit = fields.Float()
     debit = fields.Float()
     date = fields.Date()
 
     @api.model_cr
     def init(self):
-        script = opj(os.path.dirname(__file__), 'mis_unpaid_invoice.sql')
+        script = opj(os.path.dirname(__file__), "mis_unpaid_invoice.sql")
         with open(script) as f:
-            tools.drop_view_if_exists(self.env.cr, 'mis_unpaid_invoice')
+            tools.drop_view_if_exists(self.env.cr, "mis_unpaid_invoice")
             self.env.cr.execute(f.read())
