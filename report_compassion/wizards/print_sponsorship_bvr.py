75062ce7f7acd8203a893fffba4d9db008a48b2f
Sylvain Losey <35432469+SylvainLosey@users.noreply.github.com>
2020-02-21 15:54:42 +0100

    CO-2916 Après le 15 décembre, les parrainages imprimés sortent des bulletins vides (#1005)
    * CO-2916
    
    * Black

--- report_compassion/wizards/print_sponsorship_bvr.py
+++ report_compassion/wizards/print_sponsorship_bvr.py
@@ -20,28 +20,37 @@ class PrintSponsorshipBvr(models.TransientModel):
     Wizard for selecting a period and the format for printing
     payment slips of a sponsorship.
     """
-    _name = 'print.sponsorship.bvr'
 
-    period_selection = fields.Selection([
-        ('this_year', 'Current year'),
-        ('next_year', 'Next year'),
-    ], default='this_year')
-    paper_format = fields.Selection([
-        ('report_compassion.3bvr_sponsorship', '3 BVR'),
-        ('report_compassion.2bvr_sponsorship', '2 BVR'),
-        ('report_compassion.bvr_sponsorship', 'Single BVR')
-    ], default='report_compassion.3bvr_sponsorship')
+    _name = "print.sponsorship.bvr"
+
+    def _compute_default_period_selection(self):
+        # After december 15th choose next year by default, to avoid blank BVRs
+        today = datetime.today()
+        return "next_year" if today >= datetime(today.year, 12, 15) else "this_year"
+
+    period_selection = fields.Selection(
+        [("this_year", "Current year"), ("next_year", "Next year"), ],
+        default=_compute_default_period_selection,
+    )
+    paper_format = fields.Selection(
+        [
+            ("report_compassion.3bvr_sponsorship", "3 BVR"),
+            ("report_compassion.2bvr_sponsorship", "2 BVR"),
+            ("report_compassion.bvr_sponsorship", "Single BVR"),
+        ],
+        default="report_compassion.3bvr_sponsorship",
+    )
     date_start = fields.Date(default=lambda s: s.default_start())
     date_stop = fields.Date(default=lambda s: s.default_stop())
     include_gifts = fields.Boolean()
     draw_background = fields.Boolean()
-    state = fields.Selection([('new', 'new'), ('pdf', 'pdf')], default='new')
+    state = fields.Selection([("new", "new"), ("pdf", "pdf")], default="new")
     pdf = fields.Boolean()
-    pdf_name = fields.Char(default='sponsorship payment.pdf')
+    pdf_name = fields.Char(default="sponsorship payment.pdf")
     pdf_download = fields.Binary(readonly=True)
     preprinted = fields.Boolean(
-        help='Enable if you print on a payment slip that already has company '
-             'information printed on it.'
+        help="Enable if you print on a payment slip that already has company "
+        "information printed on it."
     )
 
     @api.model
@@ -54,21 +63,21 @@ class PrintSponsorshipBvr(models.TransientModel):
         today = datetime.today()
         return fields.Date.to_string(today.replace(day=31, month=12))
 
-    @api.onchange('period_selection')
+    @api.onchange("period_selection")
     def onchange_period(self):
         today = datetime.today()
         start = fields.Datetime.from_string(self.date_start)
         stop = fields.Datetime.from_string(self.date_stop)
-        if self.period_selection == 'this_year':
+        if self.period_selection == "this_year":
             start = start.replace(year=today.year)
             stop = stop.replace(year=today.year)
-        elif self.period_selection == 'next_year':
+        elif self.period_selection == "next_year":
             start = start.replace(year=today.year + 1)
             stop = stop.replace(year=today.year + 1)
         self.date_start = start
         self.date_stop = stop
 
-    @api.onchange('pdf')
+    @api.onchange("pdf")
     def onchange_pdf(self):
         if self.pdf:
             self.draw_background = True
@@ -83,40 +92,38 @@ class PrintSponsorshipBvr(models.TransientModel):
         (single bvr / 2 bvr / 3 bvr).
         :return: Generated report
         """
-        if fields.Date.from_string(self.date_start) >= \
-                fields.Date.from_string(self.date_stop):
+        if fields.Date.from_string(self.date_start) >= fields.Date.from_string(
+            self.date_stop
+        ):
             raise odooWarning(_("Date stop must be after date start."))
         data = {
-            'date_start': self.date_start,
-            'date_stop': self.date_stop,
-            'gifts': self.include_gifts,
-            'doc_ids': self.env.context.get('active_ids'),
-            'background': self.draw_background,
-            'preprinted': self.preprinted
+            "date_start": self.date_start,
+            "date_stop": self.date_stop,
+            "gifts": self.include_gifts,
+            "doc_ids": self.env.context.get("active_ids"),
+            "background": self.draw_background,
+            "preprinted": self.preprinted,
         }
-        records = self.env[self.env.context.get('active_model')].browse(
-            data['doc_ids'])
+        records = self.env[self.env.context.get("active_model")].browse(data["doc_ids"])
         if self.pdf:
-            name = records.name if len(records) == 1 else \
-                _('sponsorship payment slips')
-            self.pdf_name = name + '.pdf'
+            name = records.name if len(records) == 1 else _("sponsorship payment slips")
+            self.pdf_name = name + ".pdf"
             self.pdf_download = base64.b64encode(
-                self.env['report'].with_context(
-                    must_skip_send_to_printer=True).get_pdf(
-                        records.ids, self.paper_format, data=data))
-            self.state = 'pdf'
+                self.env["report"]
+                .with_context(must_skip_send_to_printer=True)
+                .get_pdf(records.ids, self.paper_format, data=data)
+            )
+            self.state = "pdf"
             return {
-                'name': 'Download report',
-                'type': 'ir.actions.act_window',
-                'res_model': self._name,
-                'res_id': self.id,
-                'view_mode': 'form',
-                'target': 'new',
-                'context': self.env.context,
+                "name": "Download report",
+                "type": "ir.actions.act_window",
+                "res_model": self._name,
+                "res_id": self.id,
+                "view_mode": "form",
+                "target": "new",
+                "context": self.env.context,
             }
-        return self.env['report'].get_action(
-            records.ids, self.paper_format, data
-        )
+        return self.env["report"].get_action(records.ids, self.paper_format, data)
 
 
 class PrintBvrDue(models.TransientModel):
@@ -124,12 +131,13 @@ class PrintBvrDue(models.TransientModel):
     Wizard for selecting a period and the format for printing
     payment slips of a sponsorship.
     """
-    _name = 'print.sponsorship.bvr.due'
+
+    _name = "print.sponsorship.bvr.due"
 
     draw_background = fields.Boolean()
-    state = fields.Selection([('new', 'new'), ('pdf', 'pdf')], default='new')
+    state = fields.Selection([("new", "new"), ("pdf", "pdf")], default="new")
     pdf = fields.Boolean()
-    pdf_name = fields.Char(default='sponsorship due.pdf')
+    pdf_name = fields.Char(default="sponsorship due.pdf")
     pdf_download = fields.Binary(readonly=True)
 
     @api.multi
@@ -138,28 +146,28 @@ class PrintBvrDue(models.TransientModel):
         Prepare data for the report
         :return: Generated report
         """
-        records = self.env[self.env.context.get('active_model')].browse(
-            self.env.context.get('active_ids'))
+        records = self.env[self.env.context.get("active_model")].browse(
+            self.env.context.get("active_ids")
+        )
         data = {
-            'background': self.draw_background,
-            'doc_ids': records.ids,
+            "background": self.draw_background,
+            "doc_ids": records.ids,
         }
-        report = 'report_compassion.bvr_due'
+        report = "report_compassion.bvr_due"
         if self.pdf:
             self.pdf_download = base64.b64encode(
-                self.env['report'].with_context(
-                    must_skip_send_to_printer=True).get_pdf(
-                    records.ids, report, data=data))
-            self.state = 'pdf'
+                self.env["report"]
+                .with_context(must_skip_send_to_printer=True)
+                .get_pdf(records.ids, report, data=data)
+            )
+            self.state = "pdf"
             return {
-                'name': 'Download report',
-                'type': 'ir.actions.act_window',
-                'res_model': self._name,
-                'res_id': self.id,
-                'view_mode': 'form',
-                'target': 'new',
-                'context': self.env.context,
+                "name": "Download report",
+                "type": "ir.actions.act_window",
+                "res_model": self._name,
+                "res_id": self.id,
+                "view_mode": "form",
+                "target": "new",
+                "context": self.env.context,
             }
-        return self.env['report'].get_action(
-            records.ids, report, data
-        )
+        return self.env["report"].get_action(records.ids, report, data)
