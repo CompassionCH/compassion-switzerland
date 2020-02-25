f05cbae20e4abae3c68d887036731b91fa824332
Sylvain Losey <sylvainlosey@gmail.com>
2020-02-25 11:34:57 +0100

    CO-3041
    Change OCA to CompassionCH in readme
    
    
    Black
    
    
    Update calendar_event view

--- crm_switzerland/models/interaction_resume.py
+++ crm_switzerland/models/interaction_resume.py
@@ -1,5 +1,3 @@
-# -*- coding: utf-8 -*-
-
 #    Copyright (C) 2019 Compassion CH
 #    @author: Stephane Eicher <seicher@compassion.ch>
 
@@ -8,11 +6,12 @@ from odoo import models, fields
 
 
 class InteractionResume(models.TransientModel):
-    _inherit = 'interaction.resume'
+    _inherit = "interaction.resume"
 
-    tracking_error_type = fields.Char(
-        related='email_id.tracking_event_ids.error_type')
+    tracking_error_type = fields.Char(related="email_id.tracking_event_ids.error_type")
     tracking_error_description = fields.Char(
-        related='email_id.tracking_event_ids.error_description')
+        related="email_id.tracking_event_ids.error_description"
+    )
     tracking_error_details = fields.Text(
-        related='email_id.tracking_event_ids.error_details')
+        related="email_id.tracking_event_ids.error_details"
+    )
