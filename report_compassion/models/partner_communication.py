abda3f51bee668b5d08f3fff1c0a073a337d11fc
qgigon <gigon.quentin@gmail.com>
2020-02-27 10:14:06 +0100

    print and send_mail cases handled. now need to do both

--- report_compassion/models/partner_communication.py
+++ report_compassion/models/partner_communication.py
@@ -7,6 +7,7 @@
 #    The licence is in the file __manifest__.py
 #
 ##############################################################################
+import base64
 from odoo import api, models, fields
 
 
@@ -40,11 +41,55 @@ class PartnerCommunication(models.Model):
         Update the count of succes story prints when sending a receipt.
         :return: True
         """
-        print_bvr = self.filtered(lambda j: j.send_mode == 'physical' and
-                                  j.product_id)
-        print_bvr.write({'report_id': self.env.ref(
-            'report_compassion.report_a4_bvr').id})
-        return super().send()
+        bvr_to_send = self.filtered(lambda j: j.send_mode in 'digital'
+                                    and j.product_id)
+        if bvr_to_send:
+            for bvr in bvr_to_send:
+                pdf_download = self._generate_pdf_data(bvr, background=True)
+                bvr.attachment_ids.unlink()
+                self._create_and_add_attachment(bvr, pdf_download)
+            return super(PartnerCommunication, bvr_to_send).send()
+
+        bvr_to_print = self.filtered(lambda j: j.send_mode in 'physical'
+                                     and j.product_id)
+        if bvr_to_print:
+            for bvr in bvr_to_print:
+                if bvr.report_id == self.env.ref('report_compassion.report_a4_bvr'):
+                    bvr.write({'report_id': self.env.ref(
+                        'report_compassion.report_a4_bvr').id})
+                else:
+                    pdf_download = self._generate_pdf_data(bvr, background=False)
+                    bvr.attachment_ids.unlink()
+                    self._create_and_add_attachment(bvr, pdf_download)
+            return super(PartnerCommunication, bvr_to_print).send()
+
+    def _create_and_add_attachment(self, bvr, datas):
+        attachment = self.env['ir.attachment'].create({
+            "name": bvr.report_id.name,
+            "type": "binary",
+            "datas": datas
+        })
+        bvr.write({
+            'attachment_ids': [[0, 0, {
+                "name": bvr.report_id.name,
+                "report_name": "report_compassion.a4_bvr",
+                "communication_id": bvr.id,
+                "attachment_id": attachment.id
+            }]]
+        })
+        return bvr
+
+    def _generate_pdf_data(self, bvr, background):
+        data = {
+            'doc_ids': bvr.id,
+            'product_id': bvr.product_id.id,
+            'background': background
+        }
+        report_ref = self.env.ref('report_compassion.report_a4_bvr')
+        pdf_data = report_ref.report_action(bvr, data=data)
+        return base64.encodebytes(
+            report_ref.render_qweb_pdf(
+                pdf_data['data']['doc_ids'], pdf_data['data'])[0])
 
     @api.model
     def _get_default_vals(self, vals, default_vals=None):
