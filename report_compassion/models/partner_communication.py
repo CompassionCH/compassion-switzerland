9a36dcd159fb78160cc69b4db5f5f13b1a61a630
ecino <ecino@users.noreply.github.com>
2020-03-05 14:27:54 +0100

    CO-2821 Upgrade report compassion (#1034)
    * print and send_mail cases handled. now need to do both
    
    * handled communication with send_mode == both
    
    * refactor, comments, fixes
    
    * CO-2821 most of the work is done, now need to test and check why body_html is empty
    
    * attachment are no longer unlink + refactor of attachment creation

--- report_compassion/models/partner_communication.py
+++ report_compassion/models/partner_communication.py
@@ -7,6 +7,7 @@
 #    The licence is in the file __manifest__.py
 #
 ##############################################################################
+import base64
 from odoo import api, models, fields
 
 
@@ -40,11 +41,69 @@ class PartnerCommunication(models.Model):
         Update the count of succes story prints when sending a receipt.
         :return: True
         """
-        print_bvr = self.filtered(lambda j: j.send_mode == 'physical' and
-                                  j.product_id)
-        print_bvr.write({'report_id': self.env.ref(
-            'report_compassion.report_a4_bvr').id})
-        return super().send()
+        bvr_to_send = self.filtered(lambda j: j.send_mode == 'digital'
+                                    and j.product_id)
+        bvr_to_print = self.filtered(lambda j: j.send_mode == 'physical'
+                                     and j.product_id)
+        bvr_both = self.filtered(lambda j: j.send_mode == 'both'
+                                 and j.product_id)
+
+        if bvr_both:
+            origin = self.env.context.get('origin')
+            for bvr in bvr_both:
+                if origin == 'both_email' or \
+                        (origin == 'both_print' and
+                         bvr.report_id !=
+                         self.env.ref('report_compassion.report_a4_bvr')):
+                    # email part
+                    self._put_bvr_in_attachments(
+                        bvr, background=origin == 'both_email')
+
+            return super(PartnerCommunication, bvr_both).send()
+
+        if bvr_to_send:
+            for bvr in bvr_to_send:
+                self._put_bvr_in_attachments(bvr, background=True)
+            return super(PartnerCommunication, bvr_to_send).send()
+
+        if bvr_to_print:
+            for bvr in bvr_to_print:
+                if bvr.report_id != self.env.ref('report_compassion.report_a4_bvr'):
+                    self._put_bvr_in_attachments(bvr, background=False)
+            return super(PartnerCommunication, bvr_to_print).send()
+
+    def _put_bvr_in_attachments(self, bvr, background):
+        pdf_download = self._generate_pdf_data(bvr, background)
+        return self._create_and_add_attachment(bvr, pdf_download)
+
+    def _create_and_add_attachment(self, bvr, datas):
+        attachment = self.env['ir.attachment'].create({
+            "name": bvr.report_id.name,
+            "type": "binary",
+            "datas": datas
+        })
+        comm_attachment = self.env['partner.communication.attachment'].create({
+            "name": bvr.report_id.name,
+            "report_name": "report_compassion.a4_bvr",
+            "communication_id": bvr.id,
+            "attachment_id": attachment.id
+        })
+        bvr.write({
+            'attachment_ids': [(4, comm_attachment.id)]
+        })
+        return bvr
+
+    def _generate_pdf_data(self, bvr, background):
+        data = {
+            'doc_ids': bvr.id,
+            'product_id': bvr.product_id.id,
+            'background': background
+        }
+        report_ref = self.env.ref('report_compassion.report_a4_bvr')
+        pdf_data = report_ref.report_action(bvr, data=data)
+        return base64.encodebytes(
+            report_ref.render_qweb_pdf(
+                pdf_data['data']['doc_ids'], pdf_data['data'])[0])
 
     @api.model
     def _get_default_vals(self, vals, default_vals=None):
