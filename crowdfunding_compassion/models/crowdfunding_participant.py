f4fe7863e14a8b4ad7d6fe790f9087a675bcbee4
qgigon <gigon.quentin@gmail.com>
2020-03-26 11:33:05 +0100

    views are now working

--- crowdfunding_compassion/models/crowdfunding_participant.py
+++ crowdfunding_compassion/models/crowdfunding_participant.py
@@ -8,9 +8,9 @@ class CrowdfundingParticipant(models.Model):
     _name = "crowdfunding.participant"
     _description = "Participant to one of our crowd-fundings"
 
-    project_id = fields.Many2one("crowdfunding.project")
-    partner_id = fields.Many2one("res.partner")
-    personal_motivation = fields.Text("Personal Motivation")
+    project_id = fields.Many2one("crowdfunding.project", string="Partner project")
+    partner_id = fields.Many2one("res.partner", string="Partner")
+    personal_motivation = fields.Text(string="Personal Motivation")
     product_number_goal = fields.Integer()
     product_number_reached = fields.Integer(compute="_compute_product_number_reached")
     number_sponsorships_goal = fields.Integer()
@@ -19,11 +19,11 @@ class CrowdfundingParticipant(models.Model):
     sponsorship_ids = fields.One2many("recurring.contract", "group_id", string="Sponsorships")
     invoice_line_ids = fields.One2many("account.invoice.line", "contract_id", string="Donations")
     presentation_video = fields.Char(help="Youtube/Vimeo link")
-    facebook_url = fields.Char()
-    twitter_url = fields.Char()
-    instagram_url = fields.Char()
-    personal_web_page_url = fields.Char()
-    profile_photo = fields.Binary()
+    facebook_url = fields.Char(string="Facebook link")
+    twitter_url = fields.Char(string="Twitter link")
+    instagram_url = fields.Char(string="Instagram link")
+    personal_web_page_url = fields.Char(string="Personal web page")
+    profile_photo = fields.Binary(string="Profile photo")
 
     @api.multi
     def _compute_product_number_reached(self):
--- crowdfunding_compassion/models/crowdfunding_project.py
+++ crowdfunding_compassion/models/crowdfunding_project.py
@@ -16,15 +16,15 @@ class CrowdfundingProject(models.Model):
             ("individual", _("Individual")),
             ("collective", _("Collective")),
         ],
-        required=True
+        required=True, default="individual"
     )
-    deadline = fields.Date()
-    cover_photo = fields.Binary()
-    presentation_video = fields.Char(help="Youtube or Vimeo link")
-    facebook_url = fields.Char()
-    twitter_url = fields.Char()
-    instagram_url = fields.Char()
-    personal_web_page_url = fields.Char()
+    deadline = fields.Date(string="Deadline of project", required=True)
+    cover_photo = fields.Binary(string="Cover Photo")
+    presentation_video = fields.Char(string="Youtube/Vimeo link")
+    facebook_url = fields.Char(string="Facebook link")
+    twitter_url = fields.Char(string="Twitter link")
+    instagram_url = fields.Char(string="Instagram link")
+    personal_web_page_url = fields.Char(string="Personal web page")
     product_id = fields.Many2one("product.product")  # fund
     product_number_goal = fields.Integer()
     product_number_reached = fields.Integer(compute="_compute_product_number_reached")
@@ -34,7 +34,7 @@ class CrowdfundingProject(models.Model):
     sponsorship_ids = fields.One2many("recurring.contract", "group_id", string="Sponsorships")
     invoice_line_ids = fields.One2many("account.invoice.line", "contract_id", string="Donations")
     project_owner_id = fields.Many2one("crowdfunding.participant")
-    participant_ids = fields.One2many("crowdfunding.participant", "partner_id")
+    participant_ids = fields.One2many("crowdfunding.participant", "partner_id", string="Participants")
     event_id = fields.Many2one("event.compassion")
     # doit créer un event lié automatiquement à la création du projet en remplissant le champs:
     #     nom
