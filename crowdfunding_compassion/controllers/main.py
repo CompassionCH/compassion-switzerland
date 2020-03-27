3313c8931b26eee0832c6d62bec71214c21b6826
qgigon <gigon.quentin@gmail.com>
2020-03-27 14:47:19 +0100

    CO-3164 init commit with working bootstrap test page

---
+++ crowdfunding_compassion/controllers/main.py
@@ -0,0 +1,24 @@
+##############################################################################
+#
+#    Copyright (C) 2020 Compassion CH (http://www.compassion.ch)
+#    @author: Quentin Gigon
+#
+#    The licence is in the file __manifest__.py
+#
+##############################################################################
+from odoo.addons.website_event_compassion.controllers.events_controller import (
+    EventsController,
+)
+
+from odoo.http import request, route
+
+
+class CrowdFundingWebsite(EventsController):
+
+    @route(["/project_list"], type="http", auth="user", website=True)
+    def projects_list(self, form_id=None, **kw):
+        """ Inject data for forms. """
+
+        # result = request.redirect("/my/home")
+        result = request.render("crowdfunding.project_list")
+        return self._form_redirect(result, full_page=True)
