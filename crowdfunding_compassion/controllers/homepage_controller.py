9f2dddce95ffa9dfaf4aef8b6ad580845868ab98
Sylvain <sylvainlosey@gmail.com>
2020-04-17 09:33:49 +0200

    Add method to compute fund impact

--- crowdfunding_compassion/controllers/homepage_controller.py
+++ crowdfunding_compassion/controllers/homepage_controller.py
@@ -1,7 +1,9 @@
+from datetime import datetime
+
 from odoo.http import request, route, Controller
 
 
-class ProjectController(Controller):
+class HomepageController(Controller):
     # Demo route used to display demo project
     @route("/home", auth="public", website=True)
     def homepage(self, **kwargs):
@@ -15,3 +17,32 @@ class ProjectController(Controller):
         }
 
         return request.render("crowdfunding_compassion.homepage", context)
+
+    def _compute_projects_impact(self, year, **kwargs):
+        projects = request.env['crowdfunding.project'].search([
+            ('deadline', '>=', datetime(year, 1, 1)),
+            ('deadline', '<=', datetime(year, 12, 31)),
+        ])
+
+        impact = {
+            'sponsorship': 0,
+            'toilets': 0,
+            'mothers_and_baby': 0,
+            'other': 0,
+        }
+
+        for project in projects:
+            impact['sponsorships'] += project.number_sponsorships_reached
+
+            # TODO: Add fund id
+            if project.product_id == '':
+                impact['toilets'] += project.product_number_reached
+
+            # TODO: Add fund id
+            elif project.product_id == '':
+                impact['toilets'] += project.product_number_reached
+
+            elif project.product_id:
+                impact['other'] += project.product_number_reached
+        
+        return impact
