66e19dc22ca9cf3b879460ed18fcdef3d5504f18
qgigon <gigon.quentin@gmail.com>
2020-04-28 13:25:29 +0200

    CO-3207 first commit, fund card is present, work done on impact so far section

--- crowdfunding_compassion/controllers/project_controller.py
+++ crowdfunding_compassion/controllers/project_controller.py
@@ -44,7 +44,7 @@ class ProjectController(Controller):
                 "text": f"{sponsorship.name} was sponsored",
                 "image": sponsorship.child_id.portrait,
                 "benefactor": sponsorship.correspondent_id.name,
-                "date": sponsorship.activation_date,
+                "date": sponsorship.activation_date or sponsorship.create_date.date(),
                 "time_ago": self.get_time_ago(sponsorship.create_date),
                 "anonymous": "TODO",
             }
@@ -57,7 +57,7 @@ class ProjectController(Controller):
                 "color": "grey",
                 "text": f"{int(donation.price_unit / donation.product_id.list_price)} "
                 f"{donation.product_id.crowdfunding_impact_text_passive}",
-                "image": "TODO: fund icon selector",
+                "image": donation.product_id.product_tmpl_id.image_medium,
                 "benefactor": donation.invoice_id.partner_id.name,
                 "date": donation.invoice_id.date_invoice,
                 "time_ago": self.get_time_ago(donation.invoice_id.create_date),
@@ -69,7 +69,12 @@ class ProjectController(Controller):
         # Chronological list of sponsorships and fund donations for impact display
         impact = sorted(sponsorships + donations, key=lambda x: x["date"])
 
-        return {"project": project, "impact": impact}
+        fund = request.env['product.template'].sudo().search([
+            ('activate_for_crowdfunding', '=', True),
+            ("name", "like", project.product_id.product_tmpl_id.name)
+        ])
+
+        return {"project": project, "impact": impact, "fund": fund}
 
     # Utils
     def get_time_ago(self, date):
