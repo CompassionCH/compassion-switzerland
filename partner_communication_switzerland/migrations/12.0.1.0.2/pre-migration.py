d833ba3043d91a59880c4ab966b800b6047e3933
qgigon <gigon.quentin@gmail.com>
2020-06-08 12:30:56 +0200

    fix for migration

--- partner_communication_switzerland/migrations/12.0.1.0.2/pre-migration.py
+++ partner_communication_switzerland/migrations/12.0.1.0.2/pre-migration.py
@@ -2,7 +2,7 @@ from openupgradelib import openupgrade
 
 
 @openupgrade.migrate()
-def migrate(cr, version):
+def migrate(env, version):
     if not version:
         return
-    openupgrade.load_xml(cr, "partner_communication_switzerland", "data/translator_action_rules.xml")
+    openupgrade.load_xml(env.cr, "partner_communication_switzerland", "data/translator_action_rules.xml")
--- sbc_switzerland/migrations/12.0.1.0.1/pre-migration.py
+++ sbc_switzerland/migrations/12.0.1.0.1/pre-migration.py
@@ -2,7 +2,7 @@ from openupgradelib import openupgrade
 
 
 @openupgrade.migrate()
-def migrate(cr, version):
+def migrate(env, version):
     if not version:
         return
-    openupgrade.load_xml(cr, "sbc_switzerland", "data/translator_action_rules.xml")
+    openupgrade.load_xml(env.cr, "sbc_switzerland", "data/translator_action_rules.xml")
