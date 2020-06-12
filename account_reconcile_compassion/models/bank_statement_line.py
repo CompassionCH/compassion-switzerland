3aa05b13e6504566dae2d9f29dfb9b359f9866c9
Sylvain Losey <sylvainlosey@gmail.com>
2020-06-12 14:30:10 +0200

    CO-3001 Implement Auto Reconciliation
    - Created a new method instead of overriding _apply_rules() to have cleaner code, and avoid unnecessary computation when it is called during bank statement import, which will be most of the time.
    - Added a button to the bank statement view to launch the function but might not be necessary

--- account_reconcile_compassion/models/bank_statement_line.py
+++ account_reconcile_compassion/models/bank_statement_line.py
@@ -19,6 +19,7 @@ from odoo.addons.sponsorship_compassion.models.product_names import (
 from odoo.exceptions import UserError
 from odoo.tools import float_round, mod10r
 from functools import reduce
+from odoo.addons.queue_job.job import job
 
 logger = logging.getLogger(__name__)
 
@@ -48,100 +49,6 @@ class BankStatementLine(models.Model):
                 partner_bank.write({"partner_id": vals["partner_id"]})
         return super().write(vals)
 
-    @api.multi
-    def reconciliation_widget_auto_reconcile(self, num_already_reconciled):
-        """
-        Commit at each successful reconciliation.
-        """
-        num_reconciled = 0
-        unreconciled_ids = list()
-        notifications = list()
-        for line in self:
-            try:
-                with self.env.cr.savepoint():
-                    res = super(
-                        BankStatementLine, line
-                    ).reconciliation_widget_auto_reconcile(0)
-                    num_reconciled += res["num_already_reconciled_lines"]
-                    unreconciled_ids.extend(res["st_lines_ids"])
-                    notifications.extend(res["notifications"])
-            except:
-                logger.error("Error when reconciling a statement line.")
-        return {
-            "st_lines_ids": unreconciled_ids,
-            "notifications": notifications,
-            "statement_name": False,
-            "num_already_reconciled_lines": num_reconciled + num_already_reconciled,
-        }
-
-    @api.multi
-    def auto_reconcile(self):
-        """
-        Extend automatic reconcile to allow one invoice selection when
-        multiple invoices are matching.
-        :return: account.move.line recordset of counterparts
-        """
-        self.ensure_one()
-        res = super().auto_reconcile()
-        if not res:
-            # Code copied from base account_bank_statement : L669
-            amount = self.amount_currency or self.amount
-            company_currency = self.journal_id.company_id.currency_id
-            st_line_currency = self.currency_id or self.journal_id.currency_id
-            precision = (
-                st_line_currency
-                and st_line_currency.decimal_places
-                or company_currency.decimal_places
-            )
-            params = {
-                "company_id": self.env.user.company_id.id,
-                "account_payable_receivable": (
-                    self.journal_id.default_credit_account_id.id,
-                    self.journal_id.default_debit_account_id.id,
-                ),
-                "amount": float_round(amount, precision_digits=precision),
-                "partner_id": self.partner_id.id,
-                "ref": self.ref,
-            }
-            currency = (
-                (st_line_currency and st_line_currency != company_currency)
-                and st_line_currency.id
-                or False
-            )
-            sql_query = self._get_common_sql_query()
-            sql_query += " AND aml.ref = %(ref)s AND ("
-            sql_query += currency and "amount_residual_currency" or "amount_residual"
-            sql_query += " = %(amount)s OR (acc.internal_type = 'liquidity'" " AND "
-            sql_query += (
-                currency and "amount_currency" or amount > 0 and "debit" or "credit"
-            )
-            sql_query += " = %(amount)s)) ORDER BY date_maturity asc," "aml.id asc"
-            self.env.cr.execute(sql_query, params)
-            match_recs = self.env.cr.dictfetchall()
-            if len(match_recs) == 1:
-                return self._reconcile(
-                    self.env["account.move.line"].browse(
-                        [aml.get("id") for aml in match_recs]
-                    )
-                )
-            elif len(match_recs) > 1:
-                # Take the first (current month or oldest one)
-                match_recs = self.env["account.move.line"].browse(
-                    [aml.get("id") for aml in match_recs]
-                )
-                res_sorted = list()
-                today = datetime.today().date()
-                for mv_line in match_recs:
-                    mv_date = mv_line.date_maturity or mv_line.date
-
-                    if mv_date.month == today.month and mv_date.year == today.year:
-                        res_sorted.insert(0, mv_line.id)
-                    else:
-                        res_sorted.append(mv_line.id)
-                res = match_recs.browse(res_sorted[0])
-                return self._reconcile(res)
-            return res
-
         def get_statement_line_for_reconciliation_widget(self):
             # Add partner reference for reconcile view
             res = super(
@@ -155,6 +62,7 @@ class BankStatementLine(models.Model):
     ##########################################################################
 
     @api.multi
+    @job(default_channel="root.bank_reconciliation")
     def process_reconciliation(
         self, counterpart_aml_dicts=None, payment_aml_rec=None, new_aml_dicts=None
     ):
