fbaeeacd770d6929924575ad425b08864095af47
Theyiot <pprizee@gmail.com>
2020-09-09 15:59:24 +0200

    CO-3284 Pending state is now replaced by an activity

--- partner_compassion/models/match_partner.py
+++ partner_compassion/models/match_partner.py
@@ -19,15 +19,6 @@ class MatchPartner(models.AbstractModel):
 
     _inherit = "res.partner.match"
 
-    def match_process_create_infos(self, infos, options=None):
-        create_infos = super().match_process_create_infos(infos, options)
-
-        # TODO CO-3284 replace with mail.activity assigned to a user. Do the change
-        # in top module cms_form_compassion and remove the inheritance here.
-        create_infos["state"] = "pending"
-
-        return create_infos
-
     def match_after_match(self, partner, new_partner, infos, opt):
         """
         Activate partner if it was a linked contact.
