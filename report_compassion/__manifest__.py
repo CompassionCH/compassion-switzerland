d2fd0520d8784e7d219d76f9f9a65ebc1a709a80
Theyiot <pprizee@gmail.com>
2020-09-11 10:40:57 +0200

    CO-3431 Adding migration script

--- report_compassion/__manifest__.py
+++ report_compassion/__manifest__.py
@@ -29,7 +29,7 @@
 # pylint: disable=C8101
 {
     "name": "Compassion CH PDF-Qweb Reports",
-    "version": "12.0.1.0.2",
+    "version": "12.0.1.0.3",
     "category": "Other",
     "author": "Compassion CH",
     "license": "AGPL-3",
