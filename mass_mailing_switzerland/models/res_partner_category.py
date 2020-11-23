c3781cac9ddb41f9d3fa751c76706e18b8bb35e6
Emanuel Cino <ecino@compassion.ch>
2020-11-23 17:13:25 +0100

    FIX Update mailchimp when smart tags are recomputed

--- mass_mailing_switzerland/models/res_partner_category.py
+++ mass_mailing_switzerland/models/res_partner_category.py
@@ -15,8 +15,16 @@ class ResPartnerCategory(models.Model):
 
     @api.multi
     def update_partner_tags(self):
-        # Update mailchimp when tags are recomputed.
+        # Update mailing contacts and mailchimp when tags are recomputed.
+        for tag in self:
+            tag.mapped("partner_ids.mailing_contact_ids").write({
+                "tag_ids": [(3, tag.id)]
+            })
         super().update_partner_tags()
+        for tag in self:
+            tag.mapped("partner_ids.mailing_contact_ids").write({
+                "tag_ids": [(4, tag.id)]
+            })
         self.env["mail.mass_mailing.contact"] \
             .with_delay().update_all_merge_fields_job(self.mapped("partner_ids").ids)
         return True
