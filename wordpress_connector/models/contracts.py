2e2b33c46879e9a7382a00dc7dce0893ef5f167c
Emanuel Cino <ecino@compassion.ch>
2021-02-23 16:08:59 +0100

    Place interested volunteer checkbox in view

--- wordpress_connector/models/contracts.py
+++ wordpress_connector/models/contracts.py
@@ -170,15 +170,6 @@ class Contracts(models.Model):
                 if not partner.advocate_details_id and not \
                         partner.interested_for_volunteering:
                     partner.interested_for_volunteering = True
-                    advocate_lang = partner.lang[:2]
-                    notify_user = self.env["ir.config_parameter"].get_param(
-                        f"potential_advocate_{advocate_lang}")
-                    if notify_user:
-                        partner.activity_schedule(
-                            "mail.mail_activity_data_todo",
-                            summary="Potential volunteer",
-                            note="This person wants to be involved with volunteering",
-                            user_id=notify_user)
 
             # Check origin
             internet_id = self.env.ref("utm.utm_medium_website").id
