be2c6c70e8b09ac84019a12dce253523a2fc9e3d
Emanuel Cino <ecino@compassion.ch>
2021-02-23 15:19:19 +0100

    CO-3583 Group communications sent by automations

--- partner_communication_switzerland/models/__init__.py
+++ partner_communication_switzerland/models/__init__.py
@@ -24,6 +24,5 @@ from . import staff_notifications_settings
 from . import sms_child_request
 from . import res_users
 from . import thankyou_config
-from . import ir_actions
 from . import res_partner_zoom_session
 from . import field_office
