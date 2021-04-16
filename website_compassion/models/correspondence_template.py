e168cf61397fbdb3a6d5d3001cad46ba512f342b
Emanuel Cino <ecino@compassion.ch>
2021-04-16 13:34:04 +0200

    CO-3603 Downsize template images in write letter page

--- website_compassion/models/correspondence_template.py
+++ website_compassion/models/correspondence_template.py
@@ -10,15 +10,13 @@ from odoo import models, fields
 
 
 class CorrespondenceTemplate(models.Model):
-    _inherit = "correspondence.template"
+    _inherit = ["correspondence.template", "website.published.mixin"]
+    _name = "correspondence.template"
 
     website_published = fields.Boolean()
     template_image_url = fields.Char(compute="_compute_template_image_url")
 
     def _compute_template_image_url(self):
-        web_base_url = (self.env["ir.config_parameter"].sudo()
-                        .get_param("web.external.url"))
         for template in self:
             template.template_image_url =\
-                f"{web_base_url}/web/image/{template._name}/" \
-                f"{template.id}/template_image"
+                f"/web/image/{self._name}/{template.id}/template_image?width=300"
