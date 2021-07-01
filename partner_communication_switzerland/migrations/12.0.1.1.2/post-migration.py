c2c488210b2c1ec3b1edfba1e510d228fa5e74d2
Emanuel Cino <ecino@compassion.ch>
2021-07-01 13:55:02 +0200

    Include migration for current runnning onboarding processes

--- partner_communication_switzerland/migrations/12.0.1.1.2/post-migration.py
+++ partner_communication_switzerland/migrations/12.0.1.1.2/post-migration.py
@@ -5,6 +5,12 @@ def migrate(cr, installed_version):
     if not installed_version:
         return
 
+    # Copy start_date over onboarding_start_date
+    cr.execute("""
+        UPDATE recurring_contract
+        SET onboarding_start_date = start_date
+        WHERE is_first_sponsorship = true
+    """)
     # Update data
     openupgrade.load_xml(
         cr, "partner_communication_switzerland", "data/onboarding_process.xml")
