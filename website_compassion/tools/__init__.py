59b79bd1b55894082dfd0aa6bb770c96b01a2d93
Jonathan Guerne <jonathan.guerne@gmail.com>
2021-07-01 11:21:20 +0200

    CO-3645 compress profile picture mycompassion

--- website_compassion/tools/__init__.py
+++ website_compassion/tools/__init__.py
@@ -1,13 +1,10 @@
 ##############################################################################
 #
-#    Copyright (C) 2018-2020 Compassion CH (http://www.compassion.ch)
-#    @author: Sebastien Toth <popod@me.com>
+#    Copyright (C) 2021 Compassion CH (http://www.compassion.ch)
+#    Releasing children from poverty in Jesus' name
+#    @author: Jonathan Guerne
 #
 #    The licence is in the file __manifest__.py
 #
 ##############################################################################
-
-from . import forms
-from . import controllers
-from . import models
-from . import tools
+from . import image_compression
