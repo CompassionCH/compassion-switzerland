313882ff8fb02ab7273804f73014f438ff809e4b
Emanuel Cino <ecino@compassion.ch>
2021-09-21 08:50:34 +0000

    Revert "CS-506 FIX general impact numbers"
    This reverts commit 9065fd3316ec9526f993e81f3ee35fbeaa39015c.

--- crowdfunding_compassion/controllers/homepage_controller.py
+++ crowdfunding_compassion/controllers/homepage_controller.py
@@ -53,7 +53,7 @@ class HomepageController(Controller):
             "sponsorship": sponsorship_card_content()
         }
         for fund in active_funds:
-            impact[fund.id] = {
+            impact[fund.name] = {
                 "type": "fund",
                 "value": 0,
                 "name": fund.crowdfunding_impact_text_active,
@@ -72,13 +72,13 @@ class HomepageController(Controller):
 
         for project in current_year_projects:
             impact["sponsorship"]["value"] += project.number_sponsorships_reached
-            project_fund = project.product_id.id
+            project_fund = project.product_id.name
             if project_fund in impact:
                 impact[project_fund]["value"] += project.product_number_reached
 
         for fund in active_funds:
-            if impact[fund.id]["value"] > 1:
-                impact[fund.id]["text"] = fund.crowdfunding_impact_text_passive_plural
+            if impact[fund.name]["value"] > 1:
+                impact[fund.name]["text"] = fund.crowdfunding_impact_text_passive_plural
 
         if impact["sponsorship"]["value"] > 1:
             impact["sponsorship"]["text"] = _("sponsored children")
