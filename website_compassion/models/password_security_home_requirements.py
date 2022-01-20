aaf5829da2953ba95bc7de339471b2b3d4d42218
Raphaël Margueron <raphael.margueron@gmail.com>
2022-01-20 13:26:59 +0100

    fix parser to new password message output

--- website_compassion/models/password_security_home_requirements.py
+++ website_compassion/models/password_security_home_requirements.py
@@ -8,19 +8,19 @@ class PasswordSecurityHomeRequirements(PasswordSecurityHome):
         if qcontext.get('login'):
             user = request.env["res.users"].sudo().search([("login", "=", qcontext.get('login'))])
             message = user.password_match_message()
-            lines = message.split("\r")
-            message_HTML = ""
+            lines = message.split("\n")
+            message_html = ""
             start_ul = False
             for line in lines:
-                if line[0:1] == "\n":
+                if line[0:1] == "*":
                     if not start_ul:
                         start_ul ^= True
-                        message_HTML += "<ul>"
-                    message_HTML += f"<li>{line[3:]}</li>"
+                        message_html += "<ul>"
+                    message_html += f"<li>{line[2:]}</li>"
                 else:
                     if start_ul:
                         start_ul ^= True
-                        message_HTML += "</ul>"
-                    message_HTML += line + "<br>"
-            qcontext["requirements"] = message_HTML
+                        message_html += "</ul>"
+                    message_html += line + "<br>"
+            qcontext["requirements"] = message_html
         return qcontext
