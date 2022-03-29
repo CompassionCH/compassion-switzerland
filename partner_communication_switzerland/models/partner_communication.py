e69d76e52418ef92fd7d27ef6ba2c02be7366a97
Emanuel Cino <ecino@compassion.ch>
2022-03-29 10:00:23 +0200

    CO-3940 Corrections
    - Change super method in order to reuse it

--- partner_communication_switzerland/models/partner_communication.py
+++ partner_communication_switzerland/models/partner_communication.py
@@ -52,42 +52,23 @@ class PartnerCommunication(models.Model):
         readonly=False,
     )
 
-    def get_related_contracts(self):
-        self.ensure_one()
-        object_ids = list(map(int, self.object_ids.split(",")))
-        if self.model != "recurring.contract":
-            # We're only interested in recurring contract, if the records linked to this communication are
-            # not contracts (which should not happen in the context of sponsorships waiting reminder 2,
-            # then we return an empty set
-            return self.env['recurring.contract']
-        return self.env['recurring.contract'].sudo().search([('id', "in", object_ids)])
-
-
     def schedule_call(self):
         self.ensure_one()
         user_id = self.user_id.id
-        sponsorship_reminder_2 = self.env.ref('partner_communication_switzerland.sponsorship_waiting_reminder_2')
+        sponsorship_reminder_2 = self.env.ref(
+            "partner_communication_switzerland.sponsorship_waiting_reminder_2")
 
-        # Check if we're in a sponsorship reminder 2
-        if self.config_id.name == sponsorship_reminder_2.name:
-            church_rep = self.env.ref('hr_switzerland.employee_tag_church_rep')
-            related_contracts = self.get_related_contracts()
+        if self.config_id == sponsorship_reminder_2:
+            related_contracts = self.get_objects()
             for contract in related_contracts:
-                event = contract.origin_id.event_id
-                if event.user_id:
-                    employee = self.env['hr.employee'].sudo().search([('user_id', '=', event.user_id.id)], limit=1)
-                    # Event ambassador is a church rep
-                    if employee.job_id.name == church_rep.name:
-                        user_id = event.user_id.id
+                ambassador = contract.origin_id.event_id.user_id
+                employee = self.env["hr.employee"].sudo().search([
+                    ("user_id", "=", ambassador.id)], limit=1)
+                # Event ambassador is a church rep
+                if employee.job_id.with_context(lang="en_US").name == "Church rep":
+                    user_id = ambassador.id
 
-        self.activity_schedule(
-            'mail.mail_activity_data_call',
-            summary="Call " + self.partner_id.name,
-            user_id=user_id,
-            note=f"Call {self.partner_id.name} at (phone) "
-                 f"{self.partner_phone or self.partner_mobile} regarding "
-                 f"the communication."
-        )
+        super(PartnerCommunication, self.sudo(user_id)).schedule_call()
 
     def print_letter(self, print_name, **print_options):
         """
