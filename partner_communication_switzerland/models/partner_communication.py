79bda1fd03ca2332688c3e688486b9ae30c4b710
Emanuel Cino <ecino@compassion.ch>
2022-04-26 15:44:41 +0200

    FIX bug in sponsorship QR attachment

--- partner_communication_switzerland/models/partner_communication.py
+++ partner_communication_switzerland/models/partner_communication.py
@@ -762,7 +762,7 @@ class PartnerCommunication(models.Model):
 
         # Include all active sponsorships for Permanent Order
         bv_sponsorships |= (
-            bv_sponsorships.filtered(lambda s: s.payment_mode_id == permanent_order)
+            sponsorships.filtered(lambda s: s.payment_mode_id == permanent_order)
                 .mapped("group_id.contract_ids")
                 .filtered(lambda s: s.state in ("active", "waiting"))
         )
