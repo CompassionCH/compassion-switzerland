02996f9e7948f5f84d89f1562ec43181221a31d8
guillaume hochet <guillaume.hochet@ik.me>
2022-05-03 16:20:18 +0200

    rename method and ensure correct behavior

--- partner_communication_switzerland/models/res_partner.py
+++ partner_communication_switzerland/models/res_partner.py
@@ -177,48 +177,53 @@ class ResPartner(models.Model):
             ) and partner.tax_certificate != "paper" and partner.nbmag in (
              "email", "no_mag")
 
-    @api.multi
+    @api.one
     def set_no_physical_letters(self):
         """Used from the view to manually switch to email only"""
-        self._inverse_no_physical_letter()
+        self._compute_inverse_no_physical_letter(True)
 
     def _inverse_no_physical_letter(self):
         for partner in self:
-            if partner.no_physical_letter:
-                vals = {
-                    "nbmag": "no_mag" if partner.nbmag == "no_mag" else "email",
-                    "tax_certificate": "no"
-                    if partner.tax_certificate == "no" else "only_email",
-                    "calendar": False,
-                    "christmas_card": False
-                }
-                for _field in ["global_communication_delivery_preference",
-                               "letter_delivery_preference",
-                               "photo_delivery_preference",
-                               "thankyou_preference"]:
-                    value = getattr(partner, _field)
-                    if "auto" in value or value == "both":
-                        vals[_field] = "auto_digital_only"
-                    elif value in ["physical", "digital"]:
-                        vals[_field] = "digital_only"
-                partner.write(vals)
-            else:
-                vals = {
-                    "calendar": True,
-                    "christmas_card": True
-                }
-                for _field in ["global_communication_delivery_preference",
-                               "letter_delivery_preference",
-                               "photo_delivery_preference",
-                               "thankyou_preference"]:
-                    value = getattr(partner, _field)
-                    if "only" in value:
-                        vals[_field] = value.replace("_only", "")
-                if partner.nbmag == "no_mag":
-                    vals["nbmag"] = "one"
-                if partner.tax_certificate == "only_email":
-                    vals["tax_certificate"] = "default"
-                partner.write(vals)
+            partner._compute_inverse_no_physical_letter(partner.no_physical_letter)
+
+    def _compute_inverse_no_physical_letter(self, no_physical_letters):
+        self.ensure_one()
+        if no_physical_letters:
+            vals = {
+                "nbmag": "no_mag" if self.nbmag == "no_mag" else "email",
+                "tax_certificate": "no"
+                if self.tax_certificate == "no" else "only_email",
+                "calendar": False,
+                "christmas_card": False
+            }
+            for _field in ["global_communication_delivery_preference",
+                           "letter_delivery_preference",
+                           "photo_delivery_preference",
+                           "thankyou_preference"]:
+                value = getattr(self, _field)
+                if "auto" in value or value == "both":
+                    vals[_field] = "auto_digital_only"
+                elif value in ["physical", "digital"]:
+                    vals[_field] = "digital_only"
+            self.write(vals)
+        else:
+            vals = {
+                "calendar": True,
+                "christmas_card": True
+            }
+            for _field in ["global_communication_delivery_preference",
+                           "letter_delivery_preference",
+                           "photo_delivery_preference",
+                           "thankyou_preference"]:
+                value = getattr(self, _field)
+                if "only" in value:
+                    vals[_field] = value.replace("_only", "")
+            if self.nbmag == "no_mag":
+                vals["nbmag"] = "one"
+            if self.tax_certificate == "only_email":
+                vals["tax_certificate"] = "default"
+            self.write(vals)
+
 
     def _compute_last_completed_tax_receipt(self):
         for partner in self:
