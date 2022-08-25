9ab645527802ad47e73c9f2949a236a2b48e0f40
Emanuel Cino <ecino@compassion.ch>
2022-08-25 14:05:13 +0200

    CP-115 FIX migration scripts
    - Fix various bugs

--- sbc_switzerland/migrations/12.0.2.0.0/post-migration.py
+++ sbc_switzerland/migrations/12.0.2.0.0/post-migration.py
@@ -13,7 +13,8 @@ def migrate(env, version):
     for translator in translators:
         partner = translator.partner_id
         user = env["res.users"].search([
-            "|", ("partner_id", "=", partner.id), ("login", "=", partner.email)
+            "|", ("partner_id", "=", partner.id), ("login", "=", partner.email),
+            ("active", "in", [True, False])
         ], limit=1)
         if not user:
             # TODO Create a specific communication to warn the translator about the new account
@@ -21,13 +22,27 @@ def migrate(env, version):
                 "partner_id": partner.id,
                 "login": partner.email or partner.ref
             })
-        if not ts_user_obj.search([("user_id", "=", user.id)]):
+        if not ts_user_obj.search([("user_id", "=", user.id), ("active", "in", [True, False])]):
             new_t = ts_user_obj.create([{
                 "user_id": user.id,
                 "translator_since": translator.active_since or translator.create_date,
                 "active": not translator.state == "inactive"
             }])
-            env["correspondence"].search([
+            letters = env["correspondence"].search([
                 ("translator_id", "=", partner.id)
-            ]).write({"new_translator_id": new_t.id})
-            env.cr.commit()
+            ])
+            letters.write({"new_translator_id": new_t.id})
+            for competence in letters.mapped("translation_competence_id"):
+                env["translation.user.skill"].create([{
+                    "translator_id": new_t.id,
+                    "competence_id": competence.id,
+                    "verified": True
+                }])
+
+    # Migrate letters already translated
+    env.cr.execute("UPDATE correspondence SET translation_status = 'done' WHERE new_translator_id IS NOT NULL")
+
+    # Migrate letters in translation
+    letters = env["correspondence"].search([("state", "=", "Global Partner translation queue")])
+    for l in letters:
+        l.send_local_translate()
