3de54017b94eb1e4a08c6201440d72183014b727
Simon <simon.gonzalez@bluewin.ch>
2023-04-17 15:56:28 +0200

    [MIG] gifts_payments: Module import Initial import of the module.

---
+++ gifts_payments/models/__init__.py
@@ -0,0 +1,11 @@
+##############################################################################
+#
+#    Copyright (C) 2018 Compassion CH (http://www.compassion.ch)
+#    Releasing children from poverty in Jesus' name
+#    @author: Samuel Fringeli
+#
+#    The licence is in the file __manifest__.py
+#
+##############################################################################
+
+from . import gifts_payments
