f70a5cbd154dbcde15043e561ad6960c82bad63e
Simon <simon.gonzalez@bluewin.ch>
2023-04-18 11:10:44 +0200

    [MIG] hr_switzerland: Module import Initial import of the module

---
+++ hr_switzerland/models/__init__.py
@@ -0,0 +1,16 @@
+##############################################################################
+#
+#    Copyright (C) 2017 Compassion CH (http://www.compassion.ch)
+#    Releasing children from poverty in Jesus' name
+#    @author: Emanuel Cino <ecino@compassion.ch>
+#
+#    The licence is in the file __manifest__.py
+#
+##############################################################################
+
+from . import hr_employee
+from . import hr_expense
+from . import hr_holidays
+from . import res_users
+from . import hr_contract
+from . import hr_payslip
