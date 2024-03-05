1833694c28dc258e1381f9d24c54147ba13b6632
Emanuel Cino <ecino@compassion.ch>
2024-03-05 12:39:10 +0100

    T1046 ADD migration

---
+++ report_compassion/migrations/pre-migrate.py
@@ -0,0 +1,5 @@
+from openupgradelib import openupgrade
+
+def migrate(cr, version):
+    openupgrade.add_xmlid(cr, 'report_compassion', 'new_donors_card_report',
+                          'ir.actions.report', 2069)
