1784e294083cef88de7a7d7d19f4db7b5aa04957
Emanuel Cino <ecino@compassion.ch>
2024-03-07 08:11:48 +0100

    [IMP] wordpress_connector/: pre-commit execution

--- report_compassion/migrations/pre-migrate.py
+++ report_compassion/migrations/pre-migrate.py
@@ -1,5 +1,7 @@
 from openupgradelib import openupgrade
 
+
 def migrate(cr, version):
-    openupgrade.add_xmlid(cr, 'report_compassion', 'new_donors_card_report',
-                          'ir.actions.report', 2069)
+    openupgrade.add_xmlid(
+        cr, "report_compassion", "new_donors_card_report", "ir.actions.report", 2069
+    )
