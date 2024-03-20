6fa358d5c00757e7600a5b3ea3acc2b4a8b59848
Jérémie Lang <jlang@compassion.ch>
2024-03-20 14:15:00 +0100

    14-T1198-Contacts-&-Addresses-missing-in-CRM - Resolving id call warning

--- partner_compassion/models/partner_compassion.py
+++ partner_compassion/models/partner_compassion.py
@@ -544,8 +544,9 @@ class ResPartner(models.Model):
 
         # Check if the partner country is Switzerland
         swiss_country = self.env.ref("base.ch")
+
         if vals.get(
-                'country_id') == swiss_country.id or self.country_id == swiss_country.id:
+                'country_id.id') == swiss_country.id or self.country_id.id == swiss_country.id:
 
             phone = vals.get("phone")
             phone_moved_to_mobile = False
