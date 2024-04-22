7301c8aa04a445102f0dbabc437e413021a139b0
David Wulliamoz <dwulliamoz@compassion.ch>
2024-04-22 18:20:02 +0200

    adapt jobs

--- sms_939/models/compassion_hold.py
+++ sms_939/models/compassion_hold.py
@@ -9,12 +9,8 @@
 ##############################################################################
 from odoo import models
 
-from odoo.addons.queue_job.job import job
-
-
 class Hold(models.Model):
     _inherit = "compassion.hold"
 
-    @job(default_channel="root.global_pool")
     def update_expiration_date(self, new_date):
-        self.write({"expiration_date": new_date})
+        self.with_delay().write({"expiration_date": new_date})
