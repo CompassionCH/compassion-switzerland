0973363b9c5f3313ff9e8e799f66118a5601ae15
Jérémie Lang <jlang@compassion.ch>
2024-05-07 09:49:16 +0200

    [T0517] - Fix: Remove quarters from invoice date. Adding Next two months in filters.

---
+++ account_switzerland/__manifest__.py
@@ -0,0 +1,43 @@
+##############################################################################
+#
+#       ______ Releasing children from poverty      _
+#      / ____/___  ____ ___  ____  ____ ___________(_)___  ____
+#     / /   / __ \/ __ `__ \/ __ \/ __ `/ ___/ ___/ / __ \/ __ \
+#    / /___/ /_/ / / / / / / /_/ / /_/ (__  |__  ) / /_/ / / / /
+#    \____/\____/_/ /_/ /_/ .___/\__,_/____/____/_/\____/_/ /_/
+#                        /_/
+#
+#    Copyright (C) 2016-2020 Compassion CH (http://www.compassion.ch)
+#    @author: Jérémie Lang <jlang@compassion.ch>
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+{
+    "name": "Compassion CH Account Move",
+    "version": "14.0.1.0.0",
+    "category": "Compassion",
+    "author": "Compassion CH",
+    "license": "AGPL-3",
+    "website": "https://github.com/CompassionCH/compassion-switzerland",
+    "external_dependencies": {},
+    "depends": ['base', "web", 'account'],
+    "data": [
+        'views/account_move.xml',
+    ],
+    "demo": [],
+    "installable": True,
+    "application": False,
+    "auto_install": False,
+}
