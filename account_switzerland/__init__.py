05dc9834527b0e5d0d43bb937d8cacca14adf7b8
Jérémie Lang <jlang@compassion.ch>
2024-05-07 15:59:51 +0200

    [T0517] - Add: Filters for current and last fiscal year.

---
+++ account_switzerland/__init__.py
@@ -0,0 +1,9 @@
+##############################################################################
+#
+#    Copyright (C) 2016 Compassion CH (http://www.compassion.ch)
+#    Releasing children from poverty in Jesus' name
+#    @author: Jérémie Lang <jlang@compassion.ch>
+#
+#    The licence is in the file __manifest__.py
+#
+##############################################################################
