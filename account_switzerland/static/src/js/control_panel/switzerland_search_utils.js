9063296e7011b8d6e5733e6d4b497468494fabca
Emanuel Cino <ecino@compassion.ch>
2024-05-14 10:43:13 +0200

    Run pre-commit

--- account_switzerland/static/src/js/control_panel/switzerland_search_utils.js
+++ account_switzerland/static/src/js/control_panel/switzerland_search_utils.js
@@ -1,13 +1,12 @@
-odoo.define('account_switzerland.switzerland_search_utils', function (require) {
-    "use strict";
+odoo.define("account_switzerland.switzerland_search_utils", function (require) {
+  "use strict";
 
-    var searchUtils = require('web.searchUtils');
-    var _lt = require('web.core')._lt;
+  var searchUtils = require("web.searchUtils");
 
-    // Remove quarter options
-    delete searchUtils.PERIOD_OPTIONS.fourth_quarter;
-    delete searchUtils.PERIOD_OPTIONS.third_quarter;
-    delete searchUtils.PERIOD_OPTIONS.second_quarter;
-    delete searchUtils.PERIOD_OPTIONS.first_quarter;
-    delete searchUtils.INTERVAL_OPTIONS.quarter;
+  // Remove quarter options
+  delete searchUtils.PERIOD_OPTIONS.fourth_quarter;
+  delete searchUtils.PERIOD_OPTIONS.third_quarter;
+  delete searchUtils.PERIOD_OPTIONS.second_quarter;
+  delete searchUtils.PERIOD_OPTIONS.first_quarter;
+  delete searchUtils.INTERVAL_OPTIONS.quarter;
 });
