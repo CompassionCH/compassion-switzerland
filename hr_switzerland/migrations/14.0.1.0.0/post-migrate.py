b09b96ee7788535b28a0a99752c0218ce7ce5535
Emanuel Cino <ecino@compassion.ch>
2024-05-23 14:49:22 +0200

    Run pre-commit

--- hr_switzerland/migrations/14.0.1.0.0/post-migrate.py
+++ hr_switzerland/migrations/14.0.1.0.0/post-migrate.py
@@ -3,9 +3,21 @@ from openupgradelib import openupgrade
 
 def copy_attachments(env, model_name):
     for record in env[model_name].search([]):
-        attachment = env["ir.attachment"].search([("res_model", "=", model_name), ("res_id", "=", record.id), ("res_field", "=", "image")])
+        attachment = env["ir.attachment"].search(
+            [
+                ("res_model", "=", model_name),
+                ("res_id", "=", record.id),
+                ("res_field", "=", "image"),
+            ]
+        )
         if attachment:
-            for image_size in ["image_1920", "image_1024", "image_512", "image_256", "image_128"]:
+            for image_size in [
+                "image_1920",
+                "image_1024",
+                "image_512",
+                "image_256",
+                "image_128",
+            ]:
                 attachment.copy({"res_field": image_size})
 
 
