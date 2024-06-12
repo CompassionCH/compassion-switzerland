0c0f1c79c968749ad760b75250ea472dd2d7a1b2
Jérémie Lang <jlang@compassion.ch>
2024-06-12 16:26:04 +0200

    [T1431] - Clean: Ruff format

--- partner_communication_switzerland/models/link_tracker.py
+++ partner_communication_switzerland/models/link_tracker.py
@@ -3,6 +3,7 @@
 #    Copyright (C) 2018 Compassion CH (http://www.compassion.ch)
 #    Releasing children from poverty in Jesus' name
 #    @author: Emanuel Cino <ecino@compassion.ch>
+#    @author: Jérémie Lang <jlang@compassion.ch>
 #
 #    The licence is in the file __manifest__.py
 #
