7f09aec4ad2ae165e0b0fa625165ba6dca109988
Nicolas Praz <praznicolas@gmail.com>
2024-07-18 15:34:17 +0200

    finally found where public_url was still used, fixed migration to also update ir_translations related to the email templates

--- partner_communication_switzerland/migrations/14.0.2.0.0/post-migrate.py
+++ partner_communication_switzerland/migrations/14.0.2.0.0/post-migrate.py
@@ -8,6 +8,11 @@ def migrate(env, version):
         """
         UPDATE mail_template
         SET body_html = REPLACE(body_html, 'public_url', 'get_start_url()')
-        WHERE body_html like '%public_url%';
+        WHERE body_html LIKE '%public_url%';
+        UPDATE ir_translation
+        SET src = REPLACE(src, 'public_url', 'get_start_url()'),
+            value = REPLACE(value, 'public_url', 'get_start_url()')
+        WHERE res_id IN (SELECT id FROM mail_template WHERE body_html LIKE '%get_start_url()%') 
+        AND name='mail.template,body_html';
         """,
     )
