ece53cb4082050db0d53333468d837e5e40b4b72
Nicolas Praz <praznicolas@gmail.com>
2024-07-18 11:18:44 +0200

    Added new migration to update faulty email templates, upgraded version of 'partner_communinication_switzerland' to 14.0.2.0.0

---
+++ partner_communication_switzerland/migrations/14.0.2.0.0/post-migrate.py
@@ -0,0 +1,13 @@
+from openupgradelib import openupgrade
+
+
+@openupgrade.migrate()
+def migrate(env, version):
+    openupgrade.logged_query(
+        env.cr,
+        """
+        UPDATE mail_template
+        SET body_html = REPLACE(body_html, 'public_url', 'get_start_url()')
+        WHERE body_html like '%public_url%';
+        """
+    )
