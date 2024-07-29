8456fbb4e31c101367f9f215bb3a160ed8370b54
Nicolas Praz <praznicolas@gmail.com>
2024-07-29 16:33:52 +0200

    T1634 - Fix faulty email templates (#1633)
    * Added new migration to update faulty email templates, upgraded version of 'partner_communinication_switzerland' to 14.0.2.0.0
    
    * pre-commit
    
    * finally found where public_url was still used, fixed migration to also update ir_translations related to the email templates
    
    * pre-commit
    
    * pre-commit, again...
    
    * more appropriate versioning

---
+++ partner_communication_switzerland/migrations/14.0.1.1.1/post-migrate.py
@@ -0,0 +1,19 @@
+from openupgradelib import openupgrade
+
+
+@openupgrade.migrate()
+def migrate(env, version):
+    openupgrade.logged_query(
+        env.cr,
+        """
+        UPDATE mail_template
+        SET body_html = REPLACE(body_html, 'public_url', 'get_start_url()')
+        WHERE body_html LIKE '%public_url%';
+        UPDATE ir_translation
+        SET src = REPLACE(src, 'public_url', 'get_start_url()'),
+            value = REPLACE(value, 'public_url', 'get_start_url()')
+        WHERE res_id IN
+            (SELECT id FROM mail_template WHERE body_html LIKE '%get_start_url()%')
+        AND name='mail.template,body_html';
+        """,
+    )
