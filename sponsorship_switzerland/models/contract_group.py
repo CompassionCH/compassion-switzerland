ed5a3e6b4f21abc97dfa922d146cc56e0b529c4a
Nicolas Praz <praznicolas@gmail.com>
2024-08-15 14:29:37 +0200

    pass the current contract when generating invoices

--- sponsorship_switzerland/models/contract_group.py
+++ sponsorship_switzerland/models/contract_group.py
@@ -278,10 +278,10 @@ class ContractGroup(models.Model):
     ##########################################################################
     #                             PRIVATE METHODS                            #
     ##########################################################################
-    def _build_invoice_gen_data(self, invoicing_date, invoicer, gift_wizard=False):
+    def _build_invoice_gen_data(self, invoicing_date, invoicer, contract, gift_wizard=False):
         """Inherit to add BVR ref and mandate"""
         inv_data = super()._build_invoice_gen_data(
-            invoicing_date, invoicer, gift_wizard
+            invoicing_date, invoicer, contract, gift_wizard
         )
         ref = self.with_context(lang=self.partner_id.lang)._compute_ref(
             invoicing_date, gift_wizard
