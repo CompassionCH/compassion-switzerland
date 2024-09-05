a23be5fcc87792ded50da880cf754a7f37f5c4ec
Alexis Allemann <alexis.allemann@gmail.com>
2024-09-05 16:16:04 +0200

    Bidirectional desynchronization

--- mass_mailing_switzerland/models/crm_lead.py
+++ mass_mailing_switzerland/models/crm_lead.py
@@ -8,10 +8,18 @@
 #
 ##############################################################################
 
-from odoo import models
+from odoo import api, models
 
 class Lead(models.Model):
     _inherit = "crm.lead"
 
     def _inverse_email_from(self):
         return
+
+    @api.depends('partner_id.email')
+    def _compute_email_from(self):
+        return
+
+    @api.onchange('partner_id')
+    def onchange_partner_id(self):
+        self.email_from = self.partner_id.email
