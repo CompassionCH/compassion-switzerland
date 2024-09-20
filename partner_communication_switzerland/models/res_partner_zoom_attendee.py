1c6552982815bc8c121776622b0a8b1a97930630
Nicolas Praz <praznicolas@gmail.com>
2024-09-20 11:55:48 +0200

    ruff-format

--- partner_communication_switzerland/models/res_partner_zoom_attendee.py
+++ partner_communication_switzerland/models/res_partner_zoom_attendee.py
@@ -101,8 +101,13 @@ class ZoomAttendee(models.Model):
 
             else:
                 partner_vals = self._convert_vals_for_res_partner(vals)
-                partner_id = self.env["res.partner.match"].match_values_to_partner(
-                    partner_vals, match_update=False, match_create=False).id
+                partner_id = (
+                    self.env["res.partner.match"]
+                    .match_values_to_partner(
+                        partner_vals, match_update=False, match_create=False
+                    )
+                    .id
+                )
 
                 existing_attendee = self.search(
                     [
