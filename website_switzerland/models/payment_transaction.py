8d957eaadf105ce954a0ff74017000f5e820e74c
NoeBerdoz <berdoznoe@gmail.com>
2024-09-24 16:13:24 +0200

    T1760 - DOC add note to remove after next migration if resolved

--- website_switzerland/models/payment_transaction.py
+++ website_switzerland/models/payment_transaction.py
@@ -1,3 +1,13 @@
+##############################################################################
+#
+#    Copyright (C) 2024 Compassion CH (https://www.compassion.ch)
+#    Releasing children from poverty in Jesus' name
+#    @author: Emanuel Cino <ecino@compassion.ch>
+#    @author: Noé Berdoz <nberdoz@compassion.ch>
+#
+#    The licence is in the file __manifest__.py
+#
+##############################################################################
 from odoo import models
 
 
@@ -5,7 +15,11 @@ class PaymentTransaction(models.Model):
     _inherit = "payment.transaction"
 
     def _post_process_after_done(self):
-        # T1760 FIX wrong language in context coming from the payment process page
+        """
+        T1760 FIX wrong language in context coming from the payment process page
+        Temporary fix for Odoo 14.0.
+        Must be removed after the next migration if resolved in newer Odoo version.
+        """
         available_langs = self.env["res.lang"].search([]).mapped("code")
         current_lang = self.env.lang
         if current_lang not in available_langs:
