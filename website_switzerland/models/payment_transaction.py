1588f940f066ff3a2bfb8a90edbd51d5468e0e2e
Emanuel Cino <ecino@compassion.ch>
2024-09-24 08:40:49 +0200

    T1760 FIX code

--- website_switzerland/models/payment_transaction.py
+++ website_switzerland/models/payment_transaction.py
@@ -7,10 +7,11 @@ class PaymentTransaction(models.Model):
     def _post_process_after_done(self):
         # T1760 FIX wrong language in context coming from the payment process page
         available_langs = self.env["res.lang"].search([]).mapped("code")
-        if self.env.lang not in available_langs:
+        current_lang = self.env.lang
+        if current_lang not in available_langs:
             self = self.with_context(lang=None)
             for lang in available_langs:
-                if lang.startswith(self.env.lang.split("_")[0]):
+                if lang.startswith(current_lang.split("_")[0]):
                     self = self.with_context(lang=lang)
                     break
         return super()._post_process_after_done()
