d79d594764cf40c6a5024e5f0492ba36d938932f
Niels Lachat <nlachat@compassion.ch>
2024-10-29 08:47:15 +0100

    Revert "start impl transition to secure cookies; to continue"
    This reverts commit 9c9b61ad8963299ec55cbee7279fd686e6602b25.

--- auth_external/controllers/auth.py
+++ auth_external/controllers/auth.py
@@ -1,38 +1,28 @@
-from datetime import timedelta
 import logging
 from typing import List, Tuple
-from urllib.parse import urlparse
 
 from odoo import SUPERUSER_ID, registry
 from odoo.exceptions import AccessDenied
-from odoo.http import Controller, Response, request, route
-from odoo.tools.config import config
+from odoo.http import Controller, request, route
 
 _logger = logging.getLogger(__name__)
+
 
 
 AUTH_LOGIN_ROUTE = "/auth/login"
 AUTH_REFRESH_ROUTE = "/auth/refresh"
 AUTH_LOGOUT_ROUTE = "/auth/logout"
-
-
 class AuthController(Controller):
 
     def _validate_fields_as_expected(self, fields: List[str], data: dict) -> None:
         for f in fields:
             if f not in data:
-                _logger.info(
-                    f"Request failed because field '{f}' was missing from the request data"
-                )
+                _logger.info(f"Request failed because field '{f}' was missing from the request data")
                 raise AccessDenied
         if len(fields) != len(data):
             _logger.info(f"Unexpected fields provided in request, expected {fields}")
             raise AccessDenied
 
-    def _get_current_hostname(self) -> str:
-        base_url = self.env["ir.config_parameter"].sudo().get_param("web.base.url")
-        # request.httprequest.url_root.rstrip('/') ??
-        return urlparse(base_url).hostname
 
     @route(
         route=AUTH_LOGIN_ROUTE,
@@ -43,9 +33,7 @@ class AuthController(Controller):
         cors="*",
     )
     def login(self):
-        self._validate_fields_as_expected(
-            ["login", "password", "totp"], request.jsonrequest
-        )
+        self._validate_fields_as_expected(["login", "password", "totp"], request.jsonrequest)
         login = request.jsonrequest["login"]
         password = request.jsonrequest["password"]
         totp = request.jsonrequest["totp"]
@@ -60,31 +48,7 @@ class AuthController(Controller):
         user = request.env["res.users"].browse(int(user_id))
         user = user.with_user(user)
 
-        tokens = user.generate_external_auth_token()
-        access_token = tokens["access_token"]
-        access_token_expires_at = tokens["access_token_expires_at"]
-        refresh_token = tokens["refresh_token"]
-        # {"user_id": user_id, "auth_tokens": }
-
-        tokens_config = request.env["auth_external.tokens_config"].get_singleton()
-        is_cookie_secure = not config.get("debug", False)
-        response = Response()
-
-        xmlrpc_path = "/xmlrpc/2/"
-        response.set_cookie(
-            "access_token",
-            access_token,
-            expires=access_token_expires_at,
-            httponly=True,
-            secure=is_cookie_secure,
-            domain=self._get_current_hostname(),
-            path=xmlrpc_path,
-            samesite="Strict"
-        )
-
-        # TODO Similar for refresh_token cookie
-
-        return response
+        return {"user_id": user_id, "auth_tokens": user.generate_external_auth_token()}
 
     def _validate_refresh_token(self, request) -> Tuple[str, dict]:
         """Validates that the request contains a valid, authentic refresh token.
@@ -105,7 +69,7 @@ class AuthController(Controller):
 
         if refresh_token is None:
             raise AccessDenied
-
+        
         res_users = request.env["res.users"]
         return refresh_token, res_users._check_refresh_token(refresh_token, None)
 
@@ -129,7 +93,7 @@ class AuthController(Controller):
             raise AccessDenied
 
         user = request.env["res.users"].sudo().browse(int(user_id))
-        user = user.with_user(user)  # exit sudo
+        user = user.with_user(user) # exit sudo
 
         return user.generate_external_auth_token(refresh_token)
 
@@ -155,20 +119,16 @@ class AuthController(Controller):
         user_id = payload["sub"]
         rt_model = refresh_tokens.sudo().get_by_jti(jti)
         if rt_model is None:
-            _logger.warning(
-                f"""{user_id=} requested to logout, but the given
+            _logger.warning(f"""{user_id=} requested to logout, but the given
                              refresh token ({jti=}) was not found in the
-                             database, very strange"""
-            )
+                             database, very strange""")
             raise AccessDenied
-
+        
         if rt_model.is_revoked:
-            _logger.warning(
-                f"""[RTRD] Refresh Token Reuse Detection triggered
+            _logger.warning(f"""[RTRD] Refresh Token Reuse Detection triggered
                              on logout ({jti=}, {user_id=}). Anyway, we were going to revoke
                              the token family, so no harm done (but still
-                             worrying: is there an XSS being exploited?) """
-            )
-
+                             worrying: is there an XSS being exploited?) """)
+            
         rt_model.sudo().revoke_family()
-        return True  # indicates success
+        return True # indicates success
