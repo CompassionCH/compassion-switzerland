e5f071878101ebbcd89e283cfc0883c6b9573501
AndiR27 <andi.ramiqi@hesge.ch>
2025-02-25 15:59:58 +0100

    [T2179] : Fixed the printing of QR invoice

--- report_compassion/models/account_invoice.py
+++ report_compassion/models/account_invoice.py
@@ -1,24 +1,34 @@
-import re
+from odoo import _, fields, models
+from odoo.exceptions import UserError
 
-from odoo import models, fields, api, _
-from odoo.exceptions import ValidationError, UserError
-from odoo.tools.float_utils import float_split_str
-from odoo.tools.misc import mod10r
 
 class AccountMove(models.Model):
-    _inherit = 'account.move'
+    _inherit = "account.move"
 
-    bank_account = fields.Many2one()
+    bank_account = fields.Many2one(
+        "res.partner.bank",
+        string="Bank Account",
+        help="Bank account to be used for the QR-bill.",
+    )
 
     def print_ch_qr_bill(self):
-        """ Triggered by the 'Print QR-bill' button.
-        """
+        """Triggered by the 'Print QR-bill' button."""
         self.ensure_one()
 
-        if not self.partner_bank_id._eligible_for_qr_code('ch_qr', self.partner_id, self.currency_id):
-            raise UserError(_("Cannot generate the QR-bill. Please check you have configured the address of your company and debtor. If you are using a QR-IBAN, also check the invoice's payment reference is a QR reference."))
+        if not self.partner_bank_id._eligible_for_qr_code(
+            "ch_qr", self.partner_id, self.currency_id
+        ):
+            raise UserError(
+                _(
+                    "Cannot generate the QR-bill. Please check you have configured the "
+                    "address of your company and debtor. If you are using a QR-IBAN, "
+                    "also check the invoice's payment reference is a QR reference."
+                )
+            )
 
         self.l10n_ch_isr_sent = True
         # self.bank_account = self.partner_bank_id
-        # return self.env.ref('report_compassion.report_compassion_qr').report_action(self)
-        return self.env.ref('report_compassion.report_compassion_qr_parent').report_action(self)
+        self.write({"bank_account": self.partner_bank_id.id})
+        return self.env.ref("report_compassion.report_compassion_qr").report_action(
+            self
+        )
