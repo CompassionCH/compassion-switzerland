f1d1a17e9cdbd8827593ec9395bd0d86753278fa
Emanuel Cino <ecino@compassion.ch>
2025-03-18 14:43:20 +0100

    T2241 FIX direct debit invoices cancel
    - Take code from Nordic

--- sponsorship_switzerland/models/contracts.py
+++ sponsorship_switzerland/models/contracts.py
@@ -349,3 +349,44 @@ class RecurringContracts(models.Model):
                 "LSV" in old_payment_name or "Postfinance" in old_payment_name
             ) and not ("LSV" in payment_name or "Postfinance" in payment_name):
                 contract.mandate_valid()
+
+    def _filter_open_invoices_to_cancel(self):
+        """
+        Exclude Direct Debit Order invoices,
+        to avoid cancelling invoices that are being paid.
+        :return: <account.move.line> recordset
+        """
+        invoice_lines = super()._filter_open_invoices_to_cancel()
+        modified_orders = self.env["account.payment.order"]
+        for move_line in invoice_lines:
+            payment_line = (
+                self.env["account.payment.line"]
+                .sudo()
+                .search(
+                    [
+                        ("move_line_id.move_id", "=", move_line.move_id.id),
+                        ("amount_currency", ">=", -move_line.amount_currency),
+                        ("state", "!=", "cancel"),
+                    ],
+                    order="amount_currency ASC",
+                    limit=1,
+                )
+            )
+            if payment_line.state == "draft":
+                # As the order is not yet validated, we can simply cancel the payment
+                modified_orders |= payment_line.order_id
+                if abs(payment_line.amount_currency) > abs(move_line.amount_currency):
+                    payment_line.amount_currency -= abs(move_line.amount_currency)
+                else:
+                    payment_line.unlink()
+            elif payment_line:
+                invoice_lines -= move_line
+        for order in modified_orders:
+            for contract in self:
+                order.message_post(
+                    body=f"Contract "
+                    f"<a href='{contract._notify_get_action_link('view')}'>"
+                    f"{contract.name}</a> was terminated. "
+                    f"Payment lines were adapted."
+                )
+        return invoice_lines
