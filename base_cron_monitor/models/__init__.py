8581e67e8e984457652cb2a7efe7dc4bed8d8089
Emanuel Cino <ecino@compassion.ch>
2025-05-20 07:49:19 +0200

    T2167 Improve cron monitor for avoiding transaction rollbacks
    - Use a separate log table as suggested by Alexandre from Camp2Camp

--- base_cron_monitor/models/__init__.py
+++ base_cron_monitor/models/__init__.py
@@ -1,5 +1,4 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
-from . import ir_cron
 from . import ir_actions_server
-from . import base_automation
+from . import server_action_monitor
--- base_cron_monitor/models/ir_actions_server.py
+++ base_cron_monitor/models/ir_actions_server.py
@@ -1,24 +1,37 @@
 import time
+import traceback
 from datetime import timedelta
 
-from odoo import fields, models
+from odoo import SUPERUSER_ID, api, models, registry
 
 
 class ServerAction(models.Model):
     _inherit = "ir.actions.server"
 
-    last_execution_time = fields.Char(
-        string="Last Execution Duration",
-        readonly=True,
-        help="Last execution duration of the job in seconds.",
-    )
-    last_exception = fields.Text()
-    last_exception_time = fields.Datetime()
-
     def run(self):
+        exception = False
+        res = False
+        e = None
         start_time = time.time()
-        res = super().run()
+        try:
+            res = super().run()
+        except Exception as ex:
+            exception = traceback.format_exc()
+            e = ex
         end_time = time.time()
         execution_time = timedelta(seconds=end_time - start_time)
-        self.sudo().write({"last_execution_time": str(execution_time)})
+        with api.Environment.manage():
+            with registry(self.env.cr.dbname).cursor() as new_cr:
+                # Create a new environment with new cursor database
+                new_env = api.Environment(new_cr, SUPERUSER_ID, self.env.context)
+                self.with_env(new_env).env["server.action.monitor"].create(
+                    {
+                        "server_action_id": self.id,
+                        "user_id": self.env.user.id,
+                        "execution_time": str(execution_time),
+                        "exception": exception,
+                    }
+                )
+        if exception and e:
+            raise e
         return res
