f7a2074a760b989514304826dd593fc3e3944544
Emanuel Cino <ecino@compassion.ch>
2025-06-19 14:11:24 +0200

    T2137 [DEL] Unused route since long time
    - SMS notifications 1child, etc.. were not working anyway

--- sms_939/controllers/sms_notification_controller.py
+++ sms_939/controllers/sms_notification_controller.py
@@ -20,20 +20,6 @@ _logger = logging.getLogger(__name__)
 
 
 class RestController(http.Controller):
-    @http.route("/sms/mnc/", type="http", auth="public", methods=["GET"], csrf=False)
-    def sms_notification(self, **parameters):
-        # ...base url...
-        # ?instance=pro&sender=%2B244342126&operator=orange&service=ULTIMATE&language=fr
-        # &command=FORWARD&receptionDate=2014-04-12+12%3A00%3A00.000
-        # &requestUid=sms21342314&text=COMPASSION
-        _logger.info(f"SMS Request received : {json.dumps(parameters)}")
-        notification_env = request.env["sms.notification"].with_user(SUPERUSER_ID)
-        notification_env.with_delay(
-            priority=1,
-            identity_key=f"sms.notification.{parameters.get('requestUid')}",
-        ).send_sms_answer(parameters)
-        return SmsNotificationAnswer([], costs=[]).get_answer()
-
     @http.route(
         "/sms/delivery/", type="http", auth="public", methods=["GET"], csrf=False
     )
--- sms_939/models/sms_notification.py
+++ sms_939/models/sms_notification.py
@@ -182,22 +182,3 @@ class SmsNotification(models.Model):
 
     def test_service_error(self):
         raise Exception
-
-    def send_sms_answer(self, parameters):
-        """Job for sending the SMS reply to a SMS child request.
-        :param parameters: SMS parameters received from 939 API.
-        :return: True
-        """
-        sms = self.create(
-            {
-                "instance": parameters.get("instance"),
-                "sender": parameters.get("sender"),
-                "operator": parameters.get("operator"),
-                "service": parameters.get("service"),
-                "language": parameters.get("language"),
-                "date": parser.parse(parameters.get("receptionDate")),
-                "uuid": parameters.get("requestUid"),
-                "text": parameters.get("text"),
-            }
-        )
-        sms.run_service()
