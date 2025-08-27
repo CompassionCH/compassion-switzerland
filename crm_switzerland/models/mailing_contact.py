d03ae31781f20567a5397a494a64d6201f83bc6d
Emanuel Cino <ecino@compassion.ch>
2025-08-27 12:48:48 +0200

    IMP allow enhanced global dicts

---
+++ crm_switzerland/models/mailing_contact.py
@@ -0,0 +1,11 @@
+from odoo import models
+
+class MailingContact(models.Model):
+    _inherit = "mailing.contact"
+
+    def _get_global_dict(self):
+        res = super()._get_global_dict()
+        res.update({
+            "partner": self.partner_id,
+        })
+        return res
