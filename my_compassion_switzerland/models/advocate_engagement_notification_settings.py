cb3c7412d97d06a7b31127c2f92c5f108daab1e9
Shayan <119674649+Shayan105@users.noreply.github.com>
2025-09-18 13:15:50 +0200

    Add help tags to the setting fields

--- my_compassion_switzerland/models/advocate_engagement_notification_settings.py
+++ my_compassion_switzerland/models/advocate_engagement_notification_settings.py
@@ -1,31 +1,38 @@
-from odoo import models, fields, exceptions
+from odoo import models, fields, exceptions , api
 
 
 class AdvocateEngagementNotificationSettings(models.TransientModel):
     _inherit = "res.config.settings"
+    help_field_template = "Email address to notify when an advocates engagement form is submitted in "
 
     advocate_engagement_notify_fr_email = fields.Char(
         "Adv. engagement (FR)",
         config_parameter="partner_communication_switzerland.advocate_engagement_notify_fr_email",
-        default="site_fr_participate@compassion.ch"
+        default="site_fr_participate@compassion.ch",
+        help=f"{help_field_template}French."
     )
     advocate_engagement_notify_de_email = fields.Char(
         "Adv. engagement (DE)",
         config_parameter="partner_communication_switzerland.advocate_engagement_notify_de_email",
-        default="site_it_participate@compassion.ch"
+        default="site_it_participate@compassion.ch",
+        help=f"{help_field_template}French."
     )
     advocate_engagement_notify_it_email = fields.Char(
         "Adv. engagement (IT)",
         config_parameter="partner_communication_switzerland.advocate_engagement_notify_it_email",
-        default="site_it_participate@compassion.ch"
+        default="site_it_participate@compassion.ch",
+        help=f"{help_field_template}French."
     )
     advocate_engagement_notify_default_email = fields.Char(
         "Advocate engagement (Default)",
         config_parameter="partner_communication_switzerland.advocate_engagement_notify_default_email",
-        default = "site_de_participate@compassion.ch"
+        default = "site_de_participate@compassion.ch",
+        help=f"{help_field_template} any other languages than german, french and italian."
     )
 
+
     def _is_valid_email(self, email):
+        """Validates the email format. Returns True if the format is respected."""
         import re
         if not email:
             return False
@@ -48,3 +55,29 @@ class AdvocateEngagementNotificationSettings(models.TransientModel):
                     "Invalid email for %s: %s" % (field_name, value)
                 )
         return super().set_values()
+
+    @api.model
+    def get_advocate_engagement_recipients(self):
+        """
+        Fetches the advocate engagement notification emails from system settings.
+
+        Returns:
+            dict: A dictionary mapping language codes ('fr', 'de', 'it', 'default')
+                  to their respective recipient email addresses.
+        """
+        ICP = self.env['ir.config_parameter'].sudo()
+
+        field_map = {
+                   'fr': 'advocate_engagement_notify_fr_email',
+                   'de': 'advocate_engagement_notify_de_email',
+                   'it': 'advocate_engagement_notify_it_email',
+                   'default': 'advocate_engagement_notify_default_email',
+               }
+
+        recipients = {}
+        for lang, field in field_map.items():
+            param_key = self._fields[field].config_parameter
+            value = ICP.get_param(param_key, default='')
+            recipients[lang] = value
+
+        return recipients
