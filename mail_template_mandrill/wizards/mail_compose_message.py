c4e3aa0e4921d4f257d1e735f6ca1bebd58ec79c
SlWa99 <walid.slimani99@gmail.com>
2026-01-20 10:09:54 +0100

    [T2881] MIG: mail_template_mandrill migration
    # Conflicts:
    #	.pre-commit-config.yaml
    #	mail_template_mandrill/__manifest__.py
    #	requirements.txt

--- mail_template_mandrill/wizards/mail_compose_message.py
+++ mail_template_mandrill/wizards/mail_compose_message.py
@@ -5,9 +5,9 @@ from odoo.tools.safe_eval import safe_eval
 class EmailComposeMessage(models.TransientModel):
     _inherit = "mail.compose.message"
 
-    def get_mail_values(self, res_ids):
+    def _prepare_mail_values(self, res_ids):
         """Attach mailchimp template to e-mail (in SMTP headers)"""
-        mail_values = super().get_mail_values(res_ids)
+        mail_values = super()._prepare_mail_values(res_ids)
         template = self.template_id
         mailchimp_template = template.mailchimp_template_ids.filtered(
             lambda m: m.lang == self.env.lang
