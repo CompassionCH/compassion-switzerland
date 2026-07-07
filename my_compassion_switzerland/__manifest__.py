4de60dc750eca55a659e789e6b51439812c5563e
NoeBerdoz <berdoznoe@gmail.com>
2026-07-07 11:57:54 +0200

    [MIG] T3285 my_compassion_switzerland: 18.0 install gate
    - manifest 18.0.1.0.0 installable; drop assets.xml data entry (xpath target
      gone, JS re-registration lands with the eBill wizard sub-task)
    - views: tree->list, attrs->direct attributes; settings inherit re-pointed to
      message_center_compassion.compassion_settings (settings DSL)
    - mail template body Jinja->QWeb + 18.0.1.0.0 post-migration reloading the
      noupdate record
    - product data: donation quantity fields -> amount fields
      (amount = quantity x list_price, per my_compassion 14.0.1.0.3)
    - settings model: named placeholders in translated error (pylint W8120)
    - pre-commit: module un-excluded

--- my_compassion_switzerland/__manifest__.py
+++ my_compassion_switzerland/__manifest__.py
@@ -9,7 +9,7 @@
 # pylint: disable=C8101
 {
     "name": "My Compassion Switzerland",
-    "version": "14.0.1.0.1",
+    "version": "18.0.1.0.0",
     "category": "Website",
     "author": "Daniel Palumbo",
     "development_status": "Beta",
@@ -37,8 +37,7 @@
         "templates/my2_volunteering.xml",
         "views/advocate_engagement_notification_settings_view.xml",
         "templates/my2_new_sponsorship_wizard_ebill.xml",
-        "templates/assets.xml",
     ],
-    "installable": False,
+    "installable": True,
     "auto_install": False,
 }
