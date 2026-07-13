fa4881a396581f10d939288d8dd509fbc80ba345
NoeBerdoz <berdoznoe@gmail.com>
2026-07-13 15:50:31 +0200

    [T3297] IMP: move Swiss portal settings from params to website fields

--- my_compassion_switzerland/__manifest__.py
+++ my_compassion_switzerland/__manifest__.py
@@ -9,7 +9,7 @@
 # pylint: disable=C8101
 {
     "name": "My Compassion Switzerland",
-    "version": "18.0.1.0.4",
+    "version": "18.0.1.0.5",
     "category": "Website",
     "author": "Daniel Palumbo",
     "development_status": "Beta",
@@ -31,7 +31,6 @@
         "data/product_gifts_data.xml",
         "data/engagement_types_data.xml",
         "data/mail_template_data.xml",
-        "data/config_parameter_data.xml",
         "views/advocate_engagement_view.xml",
         "templates/login_template.xml",
         "templates/contact_info.xml",
---
+++ my_compassion_switzerland/migrations/18.0.1.0.5/post-migration.py
@@ -0,0 +1,43 @@
+##############################################################################
+#
+#    Copyright (C) 2026 Compassion CH (http://www.compassion.ch)
+#    @author: Noé Berdoz <nberdoz@compassion.ch>
+#
+#    The licence is in the file __manifest__.py
+#
+##############################################################################
+import logging
+
+from odoo import SUPERUSER_ID, api
+
+_logger = logging.getLogger(__name__)
+
+SWISS_PORTAL_VALUES = {
+    "data_change_notification_email": "sds_requests@compassion.ch",
+    "sponsor_child_url": "https://compassion.ch/parrainer-un-enfant",
+}
+
+LEGACY_PARAMETER_KEYS = [
+    "my_compassion.data_change_notification_email",
+    "my_compassion.sponsor_child_url",
+]
+
+
+def migrate(cr, version):
+    """The portal settings moved from instance-global system parameters to
+    per-website fields (a multi-company instance runs one portal website per
+    country). Write the Swiss values on the MyCompassion websites and drop the
+    now-unused parameters.
+    """
+    env = api.Environment(cr, SUPERUSER_ID, {})
+    websites = env["website"].search([("is_my_compassion", "=", True)])
+    if websites:
+        websites.write(SWISS_PORTAL_VALUES)
+        _logger.info(
+            "Set the Swiss portal contact values on websites %s.", websites.ids
+        )
+    parameters = (
+        env["ir.config_parameter"].sudo().search([("key", "in", LEGACY_PARAMETER_KEYS)])
+    )
+    if parameters:
+        parameters.unlink()
