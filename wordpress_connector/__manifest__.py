b0565418871e2f2b6a0d7734daf8ac804e5c1bb8
Emanuel Cino <ecino@compassion.ch>
2026-07-20 13:08:17 +0200

    [MIG] T2907 wordpress_connector

--- wordpress_connector/__manifest__.py
+++ wordpress_connector/__manifest__.py
@@ -29,7 +29,7 @@
 # pylint: disable=C8101
 {
     "name": "Compassion CH Wordpress Connector",
-    "version": "14.0.1.1.0",
+    "version": "18.0.1.0.0",
     "category": "Social",
     "author": "Emanuel Cino",
     "license": "AGPL-3",
@@ -49,6 +49,6 @@
     ],
     "external_dependencies": {"python": ["pysftp", "pyyaml"]},
     "demo": [],
-    "installable": False,
+    "installable": True,
     "auto_install": True,
 }
--- wordpress_connector/wizards/__init__.py
+++
@@ -1,9 +0,0 @@
-##############################################################################
-#
-#    Copyright (C) 2023 Compassion CH (http://www.compassion.ch)
-#    @author: Emanuel Cino <ecino@compassion.ch>
-#
-#    The licence is in the file __manifest__.py
-#
-##############################################################################
-from . import sponsorship_form
