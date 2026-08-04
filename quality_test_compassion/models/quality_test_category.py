7f0a13499b0a5dc3e5e9c14a4a3b98f7202f6820
Alexandre Philibert <alexandre.philibert@ik.me>
2026-08-04 16:02:33 +0200

    [T3272] FEAT: Add parent-child relation between quality test categories

--- quality_test_compassion/models/quality_test_category.py
+++ quality_test_compassion/models/quality_test_category.py
@@ -1,7 +1,7 @@
 # Copyright 2026 Compassion CH
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
-from odoo import fields, models
+from odoo import api, fields, models
 
 
 class QualityTestCategory(models.Model):
@@ -10,3 +10,20 @@ class QualityTestCategory(models.Model):
     _order = "name"
 
     name = fields.Char(required=True)
+    parent_id = fields.Many2one(
+        "quality.test.category",
+        string="Parent Category",
+        ondelete="set null",
+    )
+
+    @api.depends("name", "parent_id")
+    def _compute_display_name(self):
+        for rec in self:
+            rec.display_name = rec._get_full_name()
+
+    def _get_full_name(self):
+        """Return the complete category path."""
+        self.ensure_one()
+        if self.parent_id:
+            return f"{self.parent_id._get_full_name()} / {self.name}"
+        return self.name
